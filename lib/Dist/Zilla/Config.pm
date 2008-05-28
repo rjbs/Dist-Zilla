b4b1a0a63408aa6d66a550e18b345f239ab39f86
Ricardo SIGNES <rjbs@cpan.org>
2008-05-27 22:36:16 -0400

    comment on config

--- lib/Dist/Zilla/Config.pm
+++ lib/Dist/Zilla/Config.pm
@@ -3,6 +3,21 @@ use warnings;
 package Dist::Zilla::Config;
 use base 'Config::INI::Reader';
 
-
+# This should steal liberally from App::Addex::Config, namely the "some
+# multipart values" stuff and the ability to load and instantiate plugins based
+# on the section names.
+#
+# I will almost certainly want the ability to have multiple instances of one
+# class plugin, though, so there will need to be some kind of indirection,
+# like:
+#
+# plugin = fooo
+# plugin = feee
+# [fooo]
+# class = Meta::Maga::Foo
+# ...
+# [feee]
+# class = Meta::Maga::Foo
+# ...
 
 1;
