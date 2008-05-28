e70f06a3999a0c1b09c3830f54a2395878be6837
Ricardo SIGNES <rjbs@cpan.org>
2008-05-28 09:32:41 -0400

    remove appendicular code

--- lib/Dist/Zilla/Role/FileWriter.pm
+++ lib/Dist/Zilla/Role/FileWriter.pm
@@ -4,16 +4,5 @@ use Moose::Role;
 with 'Dist::Zilla::Role::Plugin';
 requires 'write_files';
 
-sub write_content_to {
-  my ($self, $content, $file) = @_;
-
-  open my $out_fh, '>', "$file" or die "can't open $file for writing: $!";
-
-  print { $out_fh  } (ref $content ? $$content : $content)
-    or die "error printing to $file: $!";
-  
-  close $out_fh or die "error closing $file: $!";
-}
-
 no Moose::Role;
 1;
