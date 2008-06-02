fee905f2d3108f7c29aec22721294b67346c1634
Ricardo SIGNES <rjbs@cpan.org>
2008-06-01 23:56:45 -0400

    META.yml output

---
+++ lib/Dist/Zilla/Plugin/MetaYaml.pm
@@ -0,0 +1,33 @@
+package Dist::Zilla::Plugin::MetaYaml;
+use Moose;
+use Moose::Autobox;
+with 'Dist::Zilla::Role::FileWriter';
+
+our $VERSION = '0.001';
+
+use Dist::Zilla::File::InMemory;
+
+use YAML::Syck ();
+
+sub write_files {
+  my ($self, $arg) = @_;
+
+  my $meta = {
+    name     => $self->zilla->name,
+    version  => $self->zilla->version,
+    abstract => '...', # XXX figure this out -- rjbs, 2008-06-01
+    author   => $self->zilla->authors,
+    license  => $self->zilla->license->meta_yml_name,
+    requires => { $self->zilla->prereq },
+    generated_by => (ref $self) . ' version ' . $self->VERSION,
+  };
+
+  my $file = Dist::Zilla::File::InMemory->new({
+    name    => 'META.yml',
+    content => YAML::Syck::Dump($meta),
+  });
+
+  return [ $file ];
+}
+
+1;
