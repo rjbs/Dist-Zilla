5afa1f42e954bea6ebdd84fb21bfa3980a9e4fa9
Ricardo SIGNES <rjbs@cpan.org>
2008-06-02 19:33:00 -0400

    --no-tgz

--- lib/Dist/Zilla/App/Command/build.pm
+++ lib/Dist/Zilla/App/Command/build.pm
@@ -5,6 +5,10 @@ use Dist::Zilla::App -command;
 
 sub abstract { 'build your dist' }
 
+sub opt_spec {
+  [ 'tgz!', 'build a tarball (default behavior)', { default => 1 } ],
+}
+
 sub run {
   my ($self, $opt, $arg) = @_;
 
