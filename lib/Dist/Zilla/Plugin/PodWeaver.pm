efc554d96ec1c69966e98d5d92ccec47c976a5e1
Ricardo SIGNES <rjbs@cpan.org>
2008-06-03 18:35:16 -0400

    pod weaver that mostly works

--- lib/Dist/Zilla/Plugin/PodWeaver.pm
+++ lib/Dist/Zilla/Plugin/PodWeaver.pm
@@ -17,9 +17,23 @@ sub munge_pod {
   require PPI;
   my $content = $file->content;
   my $doc = PPI::Document->new(\$content);
-  return unless my $pod = $doc->find('PPI::Token::Pod');
-  warn ">>$_<<\n" for @$pod;
+  return unless my @pod = map {"$_"} @{ $doc->find('PPI::Token::Pod') || [] };
   $doc->prune('PPI::Token::Pod');
+
+  my $newpod = PPI::Document->new( \( join "\n", @pod ) );
+
+  my $end = do {
+    my $end_elem = $doc->find('PPI::Statement::End');
+    use Data::Dumper;
+    warn Dumper($end_elem);
+    join '', @{ $end_elem || [] };
+  };
+
+  $doc->prune('PPI::Statement::End');
+
+  $content = $end ? "$doc\n\n$newpod\n\n$end" : "$doc\n__END__\n$newpod\n";
+
+  $file->content($content);
 }
 
 __PACKAGE__->meta->make_immutable;
