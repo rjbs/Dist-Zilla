c43a884a00e31d7a823b715fd5f1e2181a3d9178
Ricardo SIGNES <rjbs@cpan.org>
2008-06-07 23:58:48 -0400

    munge files, not file, in main logic

--- lib/Dist/Zilla/Role/FileMunger.pm
+++ lib/Dist/Zilla/Role/FileMunger.pm
@@ -1,9 +1,17 @@
 package Dist::Zilla::Role::FileMunger;
 # ABSTRACT: something that alters a file's destination or content
 use Moose::Role;
+use Moose::Autobox;
 
 with 'Dist::Zilla::Role::Plugin';
 requires 'munge_file';
+
+sub munge_files {
+  my ($self) = @_;
+
+  $self->munge_file($_) for $self->zilla->files->flatten;
+}
+    
 
 no Moose::Role;
 1;
