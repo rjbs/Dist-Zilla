0acc02ddfa8e0757f3b27569a03d0aba7a014a54
Ricardo SIGNES <rjbs@cpan.org>
2008-06-09 22:27:30 -0400

    docs

--- lib/Dist/Zilla/Plugin/PruneFiles.pm
+++ lib/Dist/Zilla/Plugin/PruneFiles.pm
@@ -4,7 +4,28 @@ use Moose;
 use Moose::Autobox;
 with 'Dist::Zilla::Role::FilePruner';
 
+=head1 SYNOPSIS
+
+This plugin allows you to specify filenames to explicitly prune from your
+distribution.  This is useful if another plugin (maybe a FileGatherer) adds a
+bunch of files, and you only want a subset of them.
+
+In your F<dist.ini>:
+
+  [PruneFiles]
+  file = xt/release/pod-coverage.t ; pod coverage tests are for jerks
+
+=cut
+
 sub multivalue_args { qw(file) }
+
+=attr filenames
+
+This is an arrayref of filenames to be pruned from the distribution.  It's
+initialized by the C<file> argument to the plugin constructor (and, therefore,
+in the config).
+
+=cut
 
 has filenames => (
   is   => 'ro',
