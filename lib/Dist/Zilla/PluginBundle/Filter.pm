4fd0fd4304330c09ee93b7fa3542c62544e034a8
Ricardo SIGNES <rjbs@cpan.org>
2008-09-11 21:55:21 -0400

    fix call

--- lib/Dist/Zilla/PluginBundle/Filter.pm
+++ lib/Dist/Zilla/PluginBundle/Filter.pm
@@ -30,7 +30,7 @@ sub bundle_config {
   Carp::croak("no bundle given for bundle filter")
     unless my $bundle = $config->{bundle};
 
-  $bundle = Dist::Zilla::Config->_expand_package($bundle);
+  $bundle = Dist::Zilla::Util->expand_config_package_name($bundle);
 
   eval "require $bundle; 1" or die;
 
@@ -41,7 +41,7 @@ sub bundle_config {
   require List::MoreUtils;
   for my $i (reverse 0 .. $#plugins) {
     splice @plugins, $i, 1 if List::MoreUtils::any(sub {
-      $plugins[$i][0] eq Dist::Zilla::Config->_expand_package($_)
+      $plugins[$i][0] eq Dist::Zilla::Util->expand_config_package_name($_)
     }, @$remove);
   }
 
