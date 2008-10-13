86a9cb3e93ecd144cd4a28fee38035d9d5d13afe
Ricardo SIGNES <rjbs@cpan.org>
2008-10-12 22:06:59 -0400

    1+ releaser required to, like, release

--- lib/Dist/Zilla/App/Command/release.pm
+++ lib/Dist/Zilla/App/Command/release.pm
@@ -8,10 +8,13 @@ sub abstract { 'test your dist' }
 
 sub run {
   my ($self, $opt, $arg) = @_;
-  
+
+  Carp::croak("you can't release without any Releaser plugins")
+    unless my @releasers = $self->plugins_with(-Releaser)->flatten;
+
   my $tgz = $self->zilla->build_archive;
 
-  $_->release($tgz) for $self->plugins_with(-Release)->flatten;
+  $_->release($tgz) for @releasers;
 }
 
 1;
