c506136161fafae7e674b5de26331123a69d19cf
Jérôme Quelin <jquelin@gmail.com>
2009-06-29 18:36:27 +0200

    first working version

--- lib/Dist/Zilla/Plugin/AutoPrereq.pm
+++ lib/Dist/Zilla/Plugin/AutoPrereq.pm
@@ -1,59 +1,122 @@
 package Dist::Zilla::Plugin::AutoPrereq;
-# ABSTRACT: automatically extract prereq from your modules
+# ABSTRACT: automatically extract prereqs from your modules
 
 use strict;
 use warnings;
 
 use Moose;
+use version;
+
 with 'Dist::Zilla::Role::FixedPrereqs';
-
-
-# -- attributes
-
-has _prereqs => (
-    is         => 'ro',
-    isa        => 'HashRef',
-    default    => sub { {} },
-);
-
-
-# -- constructor
-
-sub new {
-    my ($class, $arg) = @_;
-
-    my $self = $class->SUPER::new({
-        '=name'  => delete $arg->{'=name'},
-        zilla    => delete $arg->{zilla},
-        _prereqs => $arg,
-    });
-}
-
 
 
 # -- public methods
 
 sub prereq {
-    my ($self, $file) = @_;
-print "foo\n";
-return;
-    return $self->_munge_perl($file) if $file->name    =~ /\.(?:pm|pl)$/i;
-    return $self->_munge_perl($file) if $file->content =~ /^#!(?:.*)perl(?:$|\s)/;
-    return;
+    my $self = shift;
+    my $files = $self->zilla->files;
+
+    # don't count modules under the dist namespace
+    my $dist  = $self->zilla->name;
+    $dist =~ s/-/::/g;
+
+    my %prereqs;
+    foreach my $file ( @$files ) {
+        # parse only perl files
+        next unless $file->name    =~ /\.(?:pm|pl|t)$/i
+            || $file->content =~ /^#!(?:.*)perl(?:$|\s)/;
+
+        # parse a file, and merge with existing prereqs
+        my %fprereqs = $self->_prereqs_in_file($file);
+        foreach my $module ( keys %fprereqs ) {
+            next if $module =~ /^$dist/;
+            my $version = $fprereqs{$module};
+
+            if ( exists $prereqs{$module} ) {
+                # prereq already found, check if versions are specified
+                if ( _looks_like_version($version) ) {
+                    my $vold = version->new( $prereqs{$module} );
+                    my $vnew = version->new( $version );
+                    $prereqs{$module} = $version if $vnew > $vold;
+                }
+                # don't bother adding the new prereq, since no version
+                # is provided: therefore, the old module has either a
+                # more important version or is already undef, and
+                # there's no need to replace it
+
+            } else {
+                # new prereq, let's add it
+                $prereqs{$module} = $version;
+            }
+        }
+    }
+
+    return \%prereqs;
 }
 
-# -- private methods
+
+#-- private methods
+
+#
+# my %prereqs = $autoprereq->_prereqs_in_file( $file );
+#
+# find the prereqs (hash of module / version) in $file (a
+# dist::zilla:file::ondisk object) and return them.
+#
+sub _prereqs_in_file {
+    my ($self, $file) = @_;
+
+    my %prereqs;
+
+    # quick analysis: find only plain use and require
+    my @use_lines =
+        grep { /^(?:use|require)\s+/ }
+        split /\n/, $file->content;
+
+    foreach my $line ( @use_lines ) {
+        $line =~ s/;$//; # trim end of statement
+        my (undef, $module, $version) = split /\s+/, $line;
+
+        # trim common pragmata
+        next if $module =~ /^(lib|strict|warnings)$/;
+
+        if ( _looks_like_version($module) ) {
+            # perl minimum version is a bit special
+            $prereqs{perl} = $module;
+        } else {
+            $prereqs{ $module } = _looks_like_version($version) ? $version : 0;
+        }
+    }
+
+    return %prereqs;
+}
+
+
+
+# -- private subs
+
+#
+# my $bool = _looks_like_version( $string );
+#
+# return true if $string somehow looks like a perl version.
+#
+sub _looks_like_version {
+    my $version = shift;
+    return defined $version &&
+        $version =~ /\Av?\d+(?:\.[\d_]+)?\z/;
+}
+
+
 
 no Moose;
-__PACKAGE__->meta->make_immutable(inline_constructor => 0);
+__PACKAGE__->meta->make_immutable;
 1;
 
 __END__
 
 =begin Pod::Coverage
 
-multivalue_args
-munge_file
+prereq
 
 =end Pod::Coverage
 
@@ -61,38 +124,27 @@ munge_file
 
 In your F<dist.ini>:
 
-    [Prepender]
-    copyright = 1
-    line = use strict;
-    line = use warnings;
+    [AutoPrereq]
 
 =head1 DESCRIPTION
 
-This plugin will prepend the specified lines in each Perl module or
-program within the distribution. For scripts having a shebang line,
-lines will be inserted just after it.
+This plugin will extract loosely your distribution prerequisites from
+your files.
 
-This is useful to enforce a set of pragmas to your files (since pragmas
-are lexical, they will be active for the whole file), or to add some
-copyright comments, as the fsf recommends.
+The extraction may not be perfect, since it will only find the plain
+lines beginning with C<use> or C<require> in your perl modules and
+scripts. It will trim the following pragamata: C<strict>, C<warnings>
+and C<lib>. It will also trim the modules under your dist namespace (eg:
+for C<Dist-Zilla>, it will trim all C<Dist::Zilla::*> prereqs found.
 
-The module accepts the following options in its F<dist.ini> section:
+The module does not accept any option (yet).
 
-=over 4
-
-=item * copyright - whether to insert a boilerplate copyright comment.
-defaults to false.
-
-=item * line - anything you want to add. may be specified multiple
-times. no default.
-
-=back
 
 =head1 BUGS
 
 Please report any bugs or feature request to
-C<< <bug-dist-zilla-plugin-prepender@rt.cpan.org> >>, or through the web interface
-at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Dist-Zilla-Plugin-Prepender>.
+C<< <bug-dist-zilla-plugin-autoprereq@rt.cpan.org> >>, or through the web interface
+at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Dist-Zilla-Plugin-AutoPrereq>.
 
 
 
