74208ca52ebbd91c1084fef8bfffa4a7f67ce284
Kent Fredric <kentfredric@gmail.com>
2009-07-23 12:06:33 +1200

    Documented 'install'

--- lib/Dist/Zilla/App/Command/install.pm
+++ lib/Dist/Zilla/App/Command/install.pm
@@ -4,11 +4,40 @@ package Dist::Zilla::App::Command::install;
 # ABSTRACT: install your dist
 use Dist::Zilla::App -command;
 
+=head1 SYNOPSIS
+
+Installs your distribution using a specified command.
+
+    dzil install [--install-command="cmd"]
+
+=cut
 sub abstract { 'install your dist' }
+
+=head1 EXAMPLE
+
+    $ dzil install
+    $ dzil install --install-command="cpan ."
+
+=cut
 
 sub opt_spec {
   [ 'install-command=s', 'command to run to install (e.g. "cpan .")' ],
 }
+
+=head1 OPTIONS
+
+=head2 --install-command
+
+This defines what command to run after building the dist in the dist dir.
+
+Any value that works with L<C<system>|perlfunc/system> is accepted.
+
+If not specified, calls
+
+    perl -MCPAN -einstall "."
+
+=cut
+
 
 sub run {
   my ($self, $opt, $arg) = @_;
