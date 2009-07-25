2ac36acd2130e815426a3b00cb9c82a73e60a7c7
Ricardo SIGNES <rjbs@cpan.org>
2009-07-24 23:26:01 -0600

    abstracts are good in the abstract!

--- lib/Dist/Zilla/Util/MVPAssembler.pm
+++ lib/Dist/Zilla/Util/MVPAssembler.pm
@@ -1,6 +1,7 @@
 package Dist::Zilla::Util::MVPAssembler;
 use Moose;
 extends 'Config::MVP::Assembler';
+# ABSTRACT: Dist::Zilla-specific subclass of Config::MVP::Assembler
 
 sub expand_package {
   my $str = Dist::Zilla::Util->expand_config_package_name($_[1]);
