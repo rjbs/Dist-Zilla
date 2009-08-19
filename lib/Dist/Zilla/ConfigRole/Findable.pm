34adf96add905b2f4896f8450b5eb193e204e3da
Ricardo SIGNES <rjbs@cpan.org>
2009-08-18 22:12:27 -0400

    rework how findable works; allow it to work for App

--- lib/Dist/Zilla/ConfigRole/Findable.pm
+++ lib/Dist/Zilla/ConfigRole/Findable.pm
@@ -3,6 +3,31 @@ use Moose::Role;
 # ABSTRACT: a config class that Dist::Zilla::Config::Finder can find
 
 requires 'can_be_found';
+requires 'default_extension';
+
+sub can_be_found {
+  my ($self, $arg) = @_;
+
+  my $config_file = $arg->{root}->file( $self->filename_from_args($arg) );
+  return -r "$config_file" and -f _;
+}
+
+sub filename_from_args {
+  my ($self, $arg) = @_;
+
+  # XXX: maybe we should detect conflicting cases -- rjbs, 2009-08-18
+  return $arg->{filename} if $arg->{filename};
+  
+  my $basename = $arg->{basename};
+  confess "no filename or basename supplied"
+    unless defined $arg->{basename} and length $arg->{basename};
+
+  my $extension = $self->default_extension;
+  my $filename = $basename;
+  $filename .= ".$extension" if defined $extension;
+
+  return $filename;
+}
 
 no Moose::Role;
 1;
