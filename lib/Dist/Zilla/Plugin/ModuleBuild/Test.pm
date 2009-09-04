f60e18ca357c06044f25c9e8f73762200ad0e70c
Kent Fredric <kentfredric@gmail.com>
2009-09-05 05:34:07 +1200

    Added Module::Build equivalent Test

---
+++ lib/Dist/Zilla/Plugin/ModuleBuild/Test.pm
@@ -0,0 +1,40 @@
+package Dist::Zilla::Plugin::ModuleBuild::Test;
+
+# ABSTRACT: TestRunner for ModuleBuild based Dists
+
+# $Id:$
+use Moose;
+use namespace::autoclean;
+with 'Dist::Zilla::Role::TestRunner';
+
+=head1 DESCRIPTION
+
+If you're using C<[ModuleBuild]>, this is likely the test-runner you want to use.
+
+=cut
+
+=head1 METHODS
+
+=head2 test
+
+  perl Build.PL
+  ./Build
+  ./Build test
+
+Is more or less what this plugin does.
+
+=cut
+
+sub test {
+  my ( $self, $target ) = @_;
+  eval {
+    ## no critic Punctuation
+    system($^X => 'Build.PL') and die "error with Makefile.PL\n";
+    system('./Build') and die "error running make\n";
+    system('./Build test') and die "error running make test\n";
+    1;
+  } or return $@;
+}
+
+1;
+
