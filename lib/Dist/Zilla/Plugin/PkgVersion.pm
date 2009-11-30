a9d02c0b86d5c20081608b7094c93c02483e8176
Ricardo Signes <rjbs@cpan.org>
2009-11-29 21:36:09 -0500

    use PPI for adding versions to packages

--- lib/Dist/Zilla/Plugin/PkgVersion.pm
+++ lib/Dist/Zilla/Plugin/PkgVersion.pm
@@ -3,6 +3,8 @@ package Dist::Zilla::Plugin::PkgVersion;
 use Moose;
 with 'Dist::Zilla::Role::FileMunger';
 
+use PPI;
+
 =head1 DESCRIPTION
 
 This plugin will add a line like the following to each package in each Perl
@@ -24,26 +26,39 @@ sub munge_file {
 sub munge_perl {
   my ($self, $file) = @_;
 
-  my $content = $file->content;
-
-  require Dist::Zilla::Util;
-  my $p = Dist::Zilla::Util::Nonpod->_new;
-  $p->read_string($content);
-  my $nonpod = $p->_nonpod;
-
-  if ($nonpod =~ /\$VERSION\s*=/) {
-    $self->log(sprintf('skipping %s: assigns to $VERSION', $file->name));
-    return;
-  }
-
   my $version = $self->zilla->version;
   Carp::croak("invalid characters in version") if $version !~ /\A[.0-9_]+\z/;
 
-  # That \x20 is my OH SO CLEVER way of thwarting the \s* above.
-  # -- rjbs, 2008-06-02
-  $content =~ s<^([{\t ]*)(package \S+;)([}\t ]*)$>
-               <$1$2\nour \$VERSION\x20= '$version';\n$3\n>mg;
-  $file->content($content);
+  my $content = $file->content;
+
+  my $document = PPI::Document->new(\$content)
+    or Carp::croak( PPI::Document->errstr );
+
+  {
+    # This is sort of stupid.  We want to see if we assign to $VERSION already.
+    # I'm sure there's got to be a better way to do this, but what the heck --
+    # this should work and isn't too slow for me. -- rjbs, 2009-11-29
+    my $code_only = $document->clone;
+    $code_only->prune("PPI::Token::$_") for qw(Comment Pod Quote Regexp);
+    if ($code_only->serialize =~ /\$VERSION\s*=/sm) {
+      $self->log(sprintf('skipping %s: assigns to $VERSION', $file->name));
+      return;
+    }
+  }
+
+  return unless my $package_stmts = $document->find('PPI::Statement::Package');
+
+  # $hack is here so that when we scan *this* document we
+  my $version_doc = PPI::Document->new(\qq{our \$VERSION = '$version';\n});
+  my @children = $version_doc->schildren;
+
+  for my $stmt (@$package_stmts) {
+    Carp::carp("error inserting version in " . $file->name)
+      unless $stmt->insert_after($children[0]->clone)
+      and    $stmt->insert_after( PPI::Token::Whitespace->new("\n") );
+  }
+
+  $file->content($document->serialize);
 }
 
 __PACKAGE__->meta->make_immutable;
