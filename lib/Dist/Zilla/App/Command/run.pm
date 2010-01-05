f129d575f29da0f221c84d01ec27876ad53a191a
Jérôme Quelin <jquelin@gmail.com>
2010-01-05 18:16:32 +0100

    introducing "dzil run"

---
+++ lib/Dist/Zilla/App/Command/run.pm
@@ -0,0 +1,86 @@
+use strict;
+use warnings;
+
+package Dist::Zilla::App::Command::run;
+# ABSTRACT: run stuff in a dir where your dist is built
+
+use Dist::Zilla::App -command;
+use Moose::Autobox;
+
+sub execute {
+  my ($self, $opts, $args) = @_;
+
+  my @builders =
+    reverse sort                                         # preferring mb over eumm
+    grep { ! $_->isa('Dist::Zilla::Plugin::Manifest') }  # not a real install tool
+    $self->zilla->plugins_with(-InstallTool)->flatten;
+    print ">>$_<<\n" for @builders;
+  Carp::croak("you can't release without any InstallTool plugins")
+    unless @builders;
+
+  require File::chdir;
+  require File::Temp;
+  require Path::Class;
+
+  # dzil-build the dist
+  my $build_root = Path::Class::dir('.build');
+  $build_root->mkpath unless -d $build_root;
+
+  my $target = Path::Class::dir( File::Temp::tempdir(DIR => $build_root) );
+  $self->log("building test distribution under $target");
+
+  $self->zilla->ensure_built_in($target);
+
+  # building the dist for real
+  my $error;
+  eval {
+    local $File::chdir::CWD = $target;
+    $error = $builders[0]->build;
+    system( @$args ) and die "error while running: @$args";
+    1;
+  } or do {
+    $error = $@;
+  };
+
+  if ( $error ) {
+    $self->log($error);
+    $self->log("left failed dist in place at $target");
+    exit 1;                     # Indicate test failure
+  } else {
+    $self->log("all's well; removing $target");
+    $target->rmtree;
+  }
+
+}
+
+1;
+__END__
+
+=head1 SYNOPSIS
+
+    $ dzil run ./bin/myscript
+    $ dzil run prove -lv t/mytest.t
+    $ dzil run ls
+
+
+=head1 DESCRIPTION
+
+This command will dzil-build your dist, then build the distribution
+and finally run a command in this directory. It's ultimately like
+doing this:
+
+    dzil build
+    rsync -avp My-Project-version/ .build/
+    cd .build
+    perl Makefile.PL            # or perl Build.PL
+    make                        # or ./Build        
+    <your command as defined by rest of params>
+
+Except for the fact it's built directly in a subdir of .build (like
+F<.build/asdf123>).
+
+A command returning with an non-zero error code will left the build
+directory behind for analysis, and dzil will exit with status 1.
+Otherwise, the build directory will be removed and dzil will exit
+with status 0.
+
