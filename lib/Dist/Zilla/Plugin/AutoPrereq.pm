7b0bd5187cdf9a09bea9edaca88c865a278e64f1
Ricardo Signes <rjbs@cpan.org>
2010-03-20 14:46:03 -0400

    clean up merge of AutoPrereq

--- lib/Dist/Zilla/Plugin/AutoPrereq.pm
+++ lib/Dist/Zilla/Plugin/AutoPrereq.pm
@@ -1,266 +1,93 @@
-use 5.010;
-use strict;
-use warnings;
-
 package Dist::Zilla::Plugin::AutoPrereq;
+use Moose;
+with 'Dist::Zilla::Role::FixedPrereqs';
 # ABSTRACT: automatically extract prereqs from your modules
 
-use Dist::Zilla::Util;
-use Moose;
-use MooseX::Has::Sugar;
+use Perl::PrereqScanner 0.100521;
 use PPI;
+use Version::Requirements 0.100630;  # merge with 0-min bug
 use version;
 
-with 'Dist::Zilla::Role::FixedPrereqs';
-
-# -- attributes
-
-# skiplist - a regex
-has skip => ( ro, predicate=>'has_skip' );
-
-
-# -- public methods
-
-sub prereq {
-    my $self = shift;
-    my $files = $self->zilla->files;
-
-    my %prereqs;
-    my @modules;
-    foreach my $file ( @$files ) {
-        # parse only perl files
-        next unless $file->name    =~ /\.(?:pm|pl|t)$/i
-            || $file->content =~ /^#!(?:.*)perl(?:$|\s)/;
-
-        # store module name, to trim it from require list later on
-        my $module = $file->name;
-        $module =~ s{^lib/}{};
-        $module =~ s{\.pm$}{};
-        $module =~ s{/}{::}g;
-        push @modules, $module;
-
-        # parse a file, and merge with existing prereqs
-        my %fprereqs = $self->_prereqs_in_file($file);
-        foreach my $module ( keys %fprereqs ) {
-            my $version = $fprereqs{$module};
-
-            if ( exists $prereqs{$module} ) {
-                # prereq already found, check if versions are specified
-                if ( _looks_like_version($version) ) {
-                    my $vold = version->new( $prereqs{$module} );
-                    my $vnew = version->new( $version );
-                    $prereqs{$module} = $version if $vnew > $vold;
-                }
-                # don't bother adding the new prereq, since no version
-                # is provided: therefore, the old module has either a
-                # more important version or is already undef, and
-                # there's no need to replace it
-
-            } else {
-                # new prereq, let's add it
-                $prereqs{$module} = $version;
-            }
-        }
-    }
-
-    # remove prereqs shipped with current dist
-    delete @prereqs{ @modules };
-
-    # remove prereqs from skiplist
-    if ( $self->has_skip && $self->skip ) {
-        my $skip = $self->skip;
-        my $re   = qr/$skip/;
-        my @deletes;
-        foreach my $k ( keys %prereqs ) {
-            push @deletes, $k if $k =~ $re;
-        }
-        delete @prereqs{ @deletes };
-    }
-
-    # we're done, return what we've found
-    return \%prereqs;
-}
-
-
-#-- private methods
-
-#
-# my %prereqs = $autoprereq->_prereqs_in_file( $file );
-#
-# find the prereqs (hash of module / version) in $file (a
-# dist::zilla:file::ondisk object) and return them.
-#
-sub _prereqs_in_file {
-    my ($self, $file) = @_;
-
-    my %prereqs;
-
-    my $p = Dist::Zilla::Util::Nonpod->_new;
-    $p->read_string( $file->content );
-    my $content = $p->_nonpod;
-    my $doc = PPI::Document->new( \$content );
-
-    # regular use and require
-    my $includes = $doc->find('Statement::Include') || [];
-    foreach my $node ( @$includes ) {
-        # minimum perl version
-        if ( $node->version ) {
-            $prereqs{perl} = $node->version;
-            next;
-        }
-
-        # skipping pragamata
-        next if $node->module ~~ [ qw{ strict warnings lib } ];
-
-        if ( $node->module ~~ [ qw{ base parent } ] ) {
-            # the content is in the 5th token
-            my $meat = $node->child(4);
-            my @parents = $meat->isa('PPI::Token::QuoteLike::Words')
-                ? ( $meat->literal )
-                : ( $meat->string  );
-            @prereqs{ @parents } = (0) x @parents;
-
-            # base is in perl core, parent isn't
-            next if $node->module eq 'base';
-        }
-
-        # regular modules
-        my $version = $node->module_version
-            ? $node->module_version->content
-            : 0;
-        $prereqs{ $node->module } = $version;
-    }
-
-    # for moose specifics, let's fetch top-level statements
-    my @statements =
-        grep { $_->child(0)->isa('PPI::Token::Word') }
-        grep { ref($_) eq 'PPI::Statement' } # no ->isa()
-        $doc->children;
-
-    # roles: with ...
-    my @roles =
-        map  { $_->child(2)->string }
-        grep { $_->child(0)->literal eq 'with' }
-        @statements;
-    @prereqs{ @roles } = (0) x @roles;
-
-    # inheritance: extends ...
-    my @bases =
-        map  { $_->string }
-        grep { $_->isa('PPI::Token::Quote') }
-        map  { $_->children }
-        grep { $_->child(0)->literal eq 'extends' }
-        @statements;
-    @prereqs{ @bases } = (0) x @bases;
-
-    return %prereqs;
-}
-
-
-# -- private subs
-
-#
-# my $bool = _looks_like_version( $string );
-#
-# return true if $string somehow looks like a perl version.
-#
-sub _looks_like_version {
-    my $version = shift;
-    return defined $version &&
-        $version =~ /\Av?\d+(?:\.[\d_]+)?\z/;
-}
-
-
-no Moose;
-__PACKAGE__->meta->make_immutable;
-1;
-
-__END__
-
-=begin Pod::Coverage
-
-prereq
-
-=end Pod::Coverage
+=for Pod::Coverage prereq
 
 =head1 SYNOPSIS
 
 In your F<dist.ini>:
 
-    [AutoPrereq]
-    skip = ^Foo|Bar$
-
+  [AutoPrereq]
+  skip = ^Foo|Bar$
 
 =head1 DESCRIPTION
 
 This plugin will extract loosely your distribution prerequisites from
-your files.
-
-The extraction may not be perfect but tries to do its best. It will
-currently find the following prereqs:
-
-=over 4
-
-=item * plain lines beginning with C<use> or C<require> in your perl
-modules and scripts. This includes minimum perl version.
-
-=item * regular inheritance declated with the C<base> and C<parent>
-pragamata.
-
-=item * L<Moose> inheritance declared with the C<extends> keyword.
-
-=item * L<Moose> roles included with the C<with> keyword.
-
-=back
+your files using L<Perl::PrereqScanner>.
 
 If some prereqs are not found, you can still add them manually with the
 L<Dist::Zilla::Plugin::Prereq> plugin.
 
-It will trim the following pragamata: C<strict>, C<warnings>, C<base>
-and C<lib>. However, C<parent> is kept, since it's not in a core module.
-
-It will also trim the modules shipped within your dist.
+This plugin will skip the modules shipped within your dist.
 
 The module accept the following options:
 
-=over 4
+=for :list
+= skip
+a regex that will remove any matching modules found from prereqs
 
-=item * skip: a regex that will remove any matching modules found
-from prereqs.
+=cut
 
-=back
+# -- attributes
 
+# skiplist - a regex
+has skip => (
+  is => 'ro',
+  predicate => 'has_skip',
+);
 
+# -- public methods
 
-=head1 SEE ALSO
+sub prereq {
+  my $self  = shift;
+  my $files = $self->zilla->files;
 
-You can look for information on this module at:
+  my $req = Version::Requirements->new;
 
-=over 4
+  my @modules;
+  foreach my $file (@$files) {
+    # parse only perl files
+    next unless $file->name =~ /\.(?:pm|pl|t)$/i
+             || $file->content =~ /^#!(?:.*)perl(?:$|\s)/;
 
-=item * Search CPAN
+    # store module name, to trim it from require list later on
+    my $module = $file->name;
+    $module =~ s{^lib/}{};
+    $module =~ s{\.pm$}{};
+    $module =~ s{/}{::}g;
+    push @modules, $module;
 
-L<http://search.cpan.org/dist/Dist-Zilla-Plugin-AutoPrereq>
+    # parse a file, and merge with existing prereqs
+    my $file_req = Perl::PrereqScanner->new->scan_string($file->content);
 
-=item * See open / report bugs
+    $req->add_requirements($file_req);
+  }
 
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Dist-Zilla-Plugin-AutoPrereq>
+  # remove prereqs shipped with current dist
+  $req->clear_requirement($_) for @modules;
 
-=item * Mailing-list (same as L<Dist::Zilla>)
+  # remove prereqs from skiplist
+  if ($self->has_skip && $self->skip) {
+    my $skip = $self->skip;
+    my $re   = qr/$skip/;
 
-L<http://www.listbox.com/subscribe/?list_id=139292>
+    foreach my $k ($req->required_modules) {
+      $req->clear_requirement($k) if $k =~ $re;
+    }
+  }
 
-=item * Git repository
+  # we're done, return what we've found
+  return $req->as_string_hash;
+}
 
-L<http://github.com/jquelin/dist-zilla-plugin-autoprereq>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Dist-Zilla-Plugin-AutoPrereq>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Dist-Zilla-Plugin-AutoPrereq>
-
-=back
+no Moose;
+__PACKAGE__->meta->make_immutable;
+1;
 
--- t/plugins/autoprereq.t
+++ t/plugins/autoprereq.t
@@ -1,45 +1,46 @@
 #!perl
-
 use strict;
 use warnings;
 
-use Dist::Zilla;
-use Path::Class;
-use Test::Deep;
-use Test::More tests => 1;
-use YAML       qw{ LoadFile };
+use Test::More 0.88;
 
+use lib 't/lib';
 
-# build fake dist
-chdir( dir('t', 'foo') );
-my $zilla = Dist::Zilla->from_config;
-$zilla->build_in;
-my $dir = dir('Foo-1.23');
+use Test::DZil;
+use YAML::Tiny;
+
+my $tzil = Dist::Zilla::Tester->from_config(
+  { dist_root => 'corpus/AutoPrereq' },
+  { },
+);
+
+$tzil->build;
 
 # check found prereqs
-my $meta = LoadFile( $dir->file('META.yml') );
-my %wanted = (
-    # DZPA::Main should not be extracted
-    'DZPA::Base::Moose1'    => 0,
-    'DZPA::Base::Moose2'    => 0,
-    'DZPA::Base::base1'     => 0,
-    'DZPA::Base::base2'     => 0,
-    'DZPA::Base::base3'     => 0,
-    'DZPA::Base::parent1'   => 0,
-    'DZPA::Base::parent2'   => 0,
-    'DZPA::Base::parent3'   => 0,
-    'DZPA::IgnoreAPI'       => 0,
-    'DZPA::IndentedRequire' => '3.45',
-    'DZPA::IndentedUse'     => '0.13',
-    'DZPA::MinVerComment'   => '0.50',
-    'DZPA::ModRequire'      => 0,
-    'DZPA::NotInDist'       => 0,
-    'DZPA::Role'            => 0,
-    'DZPA::ScriptUse'       => 0,
-    'parent'                => 0,
-    'perl'                  => 5.008,
-);
-cmp_deeply( $meta->{requires}, \%wanted, 'all requires found, but no more' );
+my $meta = YAML::Tiny->new->read($tzil->tempdir->file('build/META.yml'))->[0];
 
-# clean after ourselves
-$dir->rmtree;
+my %wanted = (
+  # DZPA::Main should not be extracted
+  'DZPA::Base::Moose1'    => 0,
+  'DZPA::Base::Moose2'    => 0,
+  'DZPA::Base::base1'     => 0,
+  'DZPA::Base::base2'     => 0,
+  'DZPA::Base::base3'     => 0,
+  'DZPA::Base::parent1'   => 0,
+  'DZPA::Base::parent2'   => 0,
+  'DZPA::Base::parent3'   => 0,
+  'DZPA::IgnoreAPI'       => 0,
+  'DZPA::IndentedRequire' => '3.45',
+  'DZPA::IndentedUse'     => '0.13',
+  'DZPA::MinVerComment'   => '0.50',
+  'DZPA::ModRequire'      => 0,
+  'DZPA::NotInDist'       => 0,
+  'DZPA::Role'            => 0,
+  'DZPA::ScriptUse'       => 0,
+  'parent'                => 0,
+  'perl'                  => 5.008,
+);
+
+is_deeply( $meta->{requires}, \%wanted, 'all requires found, but no more' );
+
+done_testing;
