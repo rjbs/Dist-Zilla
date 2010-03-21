3a83210c4415ea89820d7a1acfdbbf87292f4999
Ricardo Signes <rjbs@cpan.org>
2010-03-21 10:15:18 -0400

    for plugins that want to use filefinders

---
+++ lib/Dist/Zilla/Role/FileFinderUser.pm
@@ -0,0 +1,66 @@
+package Dist::Zilla::Role::FileFinderUser;
+use MooseX::Role::Parameterized;
+
+parameter finder_arg_names => (
+  isa => 'ArrayRef',
+  default => [ 'finder' ],
+);
+
+parameter default_finders => (
+  isa => 'ArrayRef',
+  required => 1,
+);
+
+parameter method => (
+  isa     => 'Str',
+  default => 'found_files',
+);
+
+role {
+  my ($p) = @_;
+
+  my ($finder_arg, @finder_arg_aliases) = @{ $p->finder_arg_names };
+  confess "no finder arg names given!" unless $finder_arg;
+
+  around mvp_multivalue_args => sub {
+    my ($self, $orig) = @_;
+
+    my @start = $self->$orig;
+    return (@start, $finder_arg);
+  };
+
+  if (@finder_arg_aliases) {
+    around mvp_aliases => sub {
+      my ($self, $orig) = @_;
+
+      my $start = $self->$orig;
+
+      for my $alias (@finder_arg_aliases) {
+        confess "$alias is already an alias to $start->{$alias}"
+          if exists $start->{$alias} and $orig->{$alias} ne $finder_arg;
+        $start->{ $alias } = $finder_arg;
+      }
+
+      return $start;
+    };
+  }
+
+  has $finder_arg => (
+    is  => 'ro',
+    isa => 'ArrayRef[Str]',
+    default => $p->default_finder,
+  );
+
+  method $p->method => sub {
+    my ($self) = @_;
+
+    my @filesets = map {; $self->zilla->find_files($_) }
+                   @{ $self->$finder_arg };
+
+    my %by_name = map {; $_->name, $_ } map { @$_ } @filesets;
+
+    return [ values %by_name ];
+  };
+};
+
+1;
