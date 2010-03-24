1a6706bcf1077d9947b690eb222e9e79035fb05a
Ricardo Signes <rjbs@cpan.org>
2010-03-23 22:01:12 -0400

    use this in tests, not @Classic

---
+++ lib/Dist/Zilla/PluginBundle/FakeClassic.pm
@@ -0,0 +1,28 @@
+package Dist::Zilla::PluginBundle::FakeClassic;
+# ABSTRACT: build something more or less like a "classic" CPAN dist
+use Moose;
+extends 'Dist::Zilla::PluginBundle::Classic';
+use Moose::Autobox;
+
+around bundle_config => sub {
+  my ($orig, $self, $arg) = @_;
+
+  my @config = $self->$orig($arg);
+
+  for my $i (0 .. $#config) {
+    if ($config[ $i ][1] eq 'Dist::Zilla::Plugin::UploadToCPAN') {
+      require Dist::Zilla::Plugin::FakeRelease;
+      $config[ $i ] = [
+        "$arg->{name}/FakeRelease",
+        'Dist::Zilla::Plugin::FakeRelease',
+        $config[ $i ][2]
+      ];
+    }
+  }
+
+  return @config;
+};
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
