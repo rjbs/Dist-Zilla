e68c40bb84283610860a9b8826d647a9c78944d1
Ricardo Signes <rjbs@cpan.org>
2010-05-03 23:32:33 -0400

    for making a new dist.ini

---
+++ lib/Dist/Zilla/Plugin/DistINI.pm
@@ -0,0 +1,38 @@
+package Dist::Zilla::Plugin::DistINI;
+# ABSTRACT: a plugin to add a dist.ini to newly-minted dists
+use Moose;
+with qw(Dist::Zilla::Role::FileGatherer);
+
+use Dist::Zilla::File::InMemory;
+
+sub gather_files {
+  my ($self, $arg) = @_;
+
+  my @core_attrs = qw(name authors copyright_holder);
+
+  my $license = ref $self->zilla->license;
+  if ($license =~ /^Software::License::(.+)$/) {
+    $license = $1;
+  } else {
+    $license = "=$license";
+  }
+
+  my $content = '';
+  $content .= sprintf "name    = %s\n", $self->zilla->name;
+  $content .= sprintf "author  = %s\n", $_ for @{ $self->zilla->authors };
+  $content .= sprintf "license = %s\n", $license;
+  $content .= sprintf "copyright_holder = %s\n", $self->zilla->copyright_holder;
+  $content .= sprintf "copyright_year   = %s\n", (localtime)[5] + 1900;
+  $content .= "\n";
+
+  my $file = Dist::Zilla::File::InMemory->new({
+    name    => "dist.ini",
+    content => $content,
+  });
+
+  $self->add_file($file);
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
