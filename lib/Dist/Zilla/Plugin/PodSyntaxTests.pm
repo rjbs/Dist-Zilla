2fa0dbc4499ef138f3c8808b55b4014630135ba1
Apocalypse <perl@0ne.us>
2010-05-04 12:26:13 -0700

    Bump Test::Pod prereq to 1.41 so it doesn't complain about L<text|url> anymore

--- lib/Dist/Zilla/Plugin/PodSyntaxTests.pm
+++ lib/Dist/Zilla/Plugin/PodSyntaxTests.pm
@@ -21,7 +21,7 @@ ___[ xt/release/pod-syntax.t ]___
 #!perl
 use Test::More;
 
-eval "use Test::Pod 1.00";
-plan skip_all => "Test::Pod 1.00 required for testing POD" if $@;
+eval "use Test::Pod 1.41";
+plan skip_all => "Test::Pod 1.41 required for testing POD" if $@;
 
 all_pod_files_ok();
