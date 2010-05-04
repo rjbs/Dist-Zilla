3aeaf4d751b20c0ea554ef825311adde7064947d
Ricardo Signes <rjbs@cpan.org>
2010-05-04 18:34:21 -0400

    greatly simplify GenerateFile
    rip out all the callback stuff
    change how templates are signaled
    remove some template variables

--- lib/Dist/Zilla/Plugin/GenerateFile.pm
+++ lib/Dist/Zilla/Plugin/GenerateFile.pm
@@ -1,5 +1,5 @@
 package Dist::Zilla::Plugin::GenerateFile;
-# ABSTRACT: build a custom file for inclusion in the dist
+# ABSTRACT: build a custom file from only the plugin configuration
 use Moose;
 use Moose::Autobox;
 with 'Dist::Zilla::Role::FileGatherer';
@@ -7,72 +7,40 @@ with 'Dist::Zilla::Role::TextTemplate';
 
 use Dist::Zilla::File::InMemory;
 
+=head1 SYNOPSIS
+
+In your F<dist.ini>:
+
+  [GenerateFile]
+  filename    = todo/master-plan.txt
+  is_template = 1
+  content = # Outlines the plan for world domination by {{$dist->name}}
+  content =
+  content = Item 1: Think of an idea!
+  content = Item 2: ?
+  content = Item 3: Profit!
+
 =head1 DESCRIPTION
 
 This plugin adds a file to the distribution.
 
-You can specify the content in F<dist.ini> or have it generated programmatically during run-time. Look
-at the L<callback_package> and it's related attributes for more details. It is an error to specify both
-the content and the callback, so please pick one!
-
-=head2 Plain content
-
-You can specify the content inline in the F<dist.ini> and it will be generated at run-time.
-
-In your F<dist.ini>:
-
-  [GenerateFile]
-  filename = AuthorNotes.txt
-  content = # Outlines the plan for world domination by {{$dist->name}}
-  content =
-  content = Item 1: Think of an idea!
-
-=head2 Callback system
-
-The method will be passed the L<Dist::Zilla::Plugin::GenerateFile> object, and any
-args specified. It is equivalent to C<$package::$sub( $self, @args )>
-
-In your F<dist.ini>:
-
-  [GenerateFile]
-  filename = dzil.html
-  callback_package = LWP::Simple
-  callback_sub = get
-  callback_args = http://dzil.org
-  content_nodzil = 1
+You can specify the content, as a sequence of lines, in your configuration.
+The specified content might be literal, or might be a Text::Template template.
 
 =head2 Templating of the content
 
-The content will also be run through L<Dist::Zilla>'s template processor,
-so you can do stuff like C<{{$dist->name}}> in the content.
-
-Available variables:
-
-  * dist ( The L<Dist::Zilla> object )
-  * self ( The L<Dist::Zilla::Plugin::GenerateFile> object )
-  * n ( a newline )
-  * t ( a tab )
-
-=head2 Using this plugin multiple times
-
-If you want to use this plugin multiple times, just give each section a unique name.
-
-In your F<dist.ini>:
-
-  [GenerateFile / file1]
-  filename = foo.bar
-  ...
-  [GenerateFile / file2]
-  filename = foo.baz
-  ...
+If you provide a C<is_template> parameter of "1", The content will also be run
+through Text::Template.  The variables C<$plugin> and C<$dist> will be
+provided, set to the GenerateFile plugin and the Dist::Zilla object
+respectively.
 
 =cut
 
-sub mvp_multivalue_args { qw(content callback_args) }
+sub mvp_multivalue_args { qw(content) }
 
 =attr filename
 
-The filename you want to generate, required.
+This attribute names the file you want to generate.  It is required.
 
 =cut
 
@@ -84,125 +52,48 @@ has filename => (
 
 =attr content
 
-The content you want to be put in the file. Specify it multiple times to get multi-line content!
+The C<content> attribute is an arrayref of lines that will be joined together
+with newlines to form the file content.
 
 =cut
 
 has content => (
-  is => 'ro',
+  is  => 'ro',
   isa => 'ArrayRef',
-  predicate => 'has_content',
 );
 
-=attr callback_package
+=attr is_template
 
-The package to use when utilizing the callback mechanism to generate the content.
+This attribute is a bool indicating whether or not the content should be
+treated as a Text::Template template.  By default, it is false.
 
 =cut
 
-has callback_package => (
-  is => 'ro',
-  isa => 'Str',
-  predicate => 'has_cb_package',
-);
-
-=attr callback_sub
-
-The subroutine name to execute on the package for the content.
-
-=cut
-
-has callback_sub => (
-  is => 'ro',
-  isa => 'Str',
-  predicate => 'has_cb_sub',
-);
-
-=attr callback_args
-
-Extra args you want to be passed to the callback. Specify it multiple times to get
-an array of args to pass to the callback.
-
-=cut
-
-has callback_args => (
-  is => 'ro',
-  isa => 'ArrayRef',
-  predicate => 'has_cb_args',
-);
-
-=attr content_nodzil
-
-Enabling this value means no L<Dist::Zilla>-specific processing
-will be done on the content.
-
-As a result, this plugin will behave a bit differently:
-
-  - The dzil object will not be passed as the first argument to the callback
-  - The returned string will not be processed by dzil's template system
-
-=cut
-
-has content_nodzil => (
-  is => 'ro',
+has is_template => (
+  is  => 'ro',
   isa => 'Bool',
   default => 0,
 );
 
-sub BUILD {
-  my $self = shift;
-
-  if ( ! $self->has_content and ! $self->has_cb_package ) {
-    $self->log_fatal( 'GenerateFile needs content or a callback' );
-  }
-
-  if ( $self->has_cb_package and ! $self->has_cb_sub ) {
-    $self->log_fatal( 'GenerateFile needs a callback_sub to call on callback_package' );
-  }
-}
-
 sub gather_files {
   my ($self, $arg) = @_;
 
-  # get the content or execute the callback?
-  my $content;
-  if ( $self->has_content ) {
-    $content = join "\n", $self->content->flatten;
-  } else {
-    Class::MOP::load_class($self->callback_package);
+  my $content = join "\n", $self->content->flatten;
+  $content .= q{\\};
 
-    # TODO this needs to be rewritten to use $pkg->can but I never could get it to work...
-    my $sub = \&{ $self->callback_package . '::' . $self->callback_sub };
-    $self->log_fatal( "Invalid callback_sub='" . $self->callback_sub . "' in callback_package='" .
-      $self->callback_package . "'" ) if ! defined &$sub;
-
-    $content = eval {
-      $sub->(
-        ( $self->content_nodzil ? () : $self ),
-        ( $self->has_cb_args ? $self->callback_args->flatten : () )
-      );
-    };
-    die $@ if $@;
-  }
-
-  my $data;
-  if ( ! $self->content_nodzil ) {
-    $data = $self->fill_in_string(
+  if ($self->is_template) {
+    $content = $self->fill_in_string(
       $content,
       {
-        dist => \($self->zilla),
-        self => \($self),
-        n => "\n",
-        t => "\t",
+        dist   => \($self->zilla),
+        plugin => \($self),
       },
     );
-  } else {
-    $data = $content;
   }
 
   my $file = Dist::Zilla::File::InMemory->new({
-    content => $data,
     name    => $self->filename,
+    content => $content,
   });
 
   $self->add_file($file);
