561eb0b40dba0df1fd7970400eb2ab16bbf58768
Christian Walde <walde.christian@googlemail.com>
2010-05-12 03:58:14 +0200

    use $^X to call ./Build when building
    DZP::MB needs to call the perl interpreter explicitly when building because
    Windows does not know how to deal with shebang.

--- lib/Dist/Zilla/Plugin/ModuleBuild.pm
+++ lib/Dist/Zilla/Plugin/ModuleBuild.pm
@@ -136,7 +136,7 @@ sub build {
   my $self = shift;
 
   system($^X => 'Build.PL') and die "error with Build.PL\n";
-  system('./Build')         and die "error running ./Build\n";
+  system($^X, 'Build')      and die "error running $^X Build\n";
 
   return;
 }
@@ -145,7 +145,7 @@ sub test {
   my ($self, $target) = @_;
 
   $self->build;
-  system('./Build test') and die "error running ./Build test\n";
+  system($^X, 'Build', 'test') and die "error running $^X Build test\n";
 
   return;
 }
