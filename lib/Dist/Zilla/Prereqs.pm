ad512498325bb9fb4683bef15189bd862d4d3edb
Ricardo Signes <rjbs@cpan.org>
2010-05-18 22:49:01 -0400

    prereqs docs

--- lib/Dist/Zilla/Prereqs.pm
+++ lib/Dist/Zilla/Prereqs.pm
@@ -12,6 +12,29 @@ use Version::Requirements;
 
 use namespace::autoclean;
 
+=head1 DESCRIPTION
+
+Dist::Zilla::Prereqs is a subcomponent of Dist::Zilla.  The C<prereqs>
+attribute on your Dist::Zilla object is a Dist::Zilla::Prereqs object, and is
+responsible for keeping track of the distribution's prerequisites.
+
+In fact, a Dist::Zilla::Prereqs object is just a thin layer over a
+L<CPAN::Meta::Prereqs> object, stored in the C<cpan_meta_prereqs> attribute.
+
+Almost everything this object does is proxied to the CPAN::Meta::Prereqs
+object, so you should really read how I<that> works.
+
+Dist::Zilla::Prereqs proxies the following methods to the CPAN::Meta::Prereqs
+object:
+
+=for :list
+* finalize
+* is_finalized
+* requirements_for
+* as_string_hash
+
+=cut
+
 has cpan_meta_prereqs => (
   is  => 'ro',
   isa => 'CPAN::Meta::Prereqs',
@@ -24,6 +47,27 @@ has cpan_meta_prereqs => (
     as_string_hash
   ) ],
 );
+
+=method register_prereqs
+
+  $prereqs->register_prereqs(%prereqs);
+
+  $prereqs->register_prereqs(\%arg, %prereqs);
+
+This method adds new minimums to the prereqs object.  If a hashref is the first
+arg, it may have entries for C<phase> and C<type> to indicate what kind of
+prereqs are being registered.  (For more information on phase and type, see
+L<CPAN::Meta::Spec>.)  For example, you might say:
+
+  $prereqs->register_prereqs(
+    { phase => 'test', type => 'recommends' },
+    'Test::Foo' => '1.23',
+    'XML::YZZY' => '2.01',
+  );
+
+If not given, phase and type default to runtime and requires, respectively.
+
+=cut
 
 sub register_prereqs {
   my $self = shift;
