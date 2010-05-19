e09b156157942a8a99355fa6787dec2f2861e976
Ricardo Signes <rjbs@cpan.org>
2010-05-18 22:53:35 -0400

    Types "docs"

--- lib/Dist/Zilla/Types.pm
+++ lib/Dist/Zilla/Types.pm
@@ -1,6 +1,16 @@
 package Dist::Zilla::Types;
 # ABSTRACT: dzil-specific type library
 
+=head1 OVERVIEW
+
+This library provides L<MooseX::Types> types for use by Dist::Zilla.  These
+types are not (yet?) for public consumption, and you should not rely on them.
+
+Dist::Zilla uses a number of types found in L<MooseX::Types::Perl>.  Maybe
+that's what you want.
+
+=cut
+
 use MooseX::Types -declare => [qw(License)];
 use MooseX::Types::Moose qw(Str);
 
