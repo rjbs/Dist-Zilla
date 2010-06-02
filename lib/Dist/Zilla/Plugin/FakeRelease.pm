74ef7cbd57afc13cacdbca157f88f78c2f8f5d69
Kent Fredric <kentfredric@gmail.com>
2010-06-03 09:47:42 +1200

    Document 'user' parameter to FakeRelease

--- lib/Dist/Zilla/Plugin/FakeRelease.pm
+++ lib/Dist/Zilla/Plugin/FakeRelease.pm
@@ -30,6 +30,11 @@ __PACKAGE__->meta->make_immutable;
 
 __END__
 
+=head1 SYNOPSIS
+
+    [FakeRelease]
+    user = CPANAUTHORID ; # optional.
+
 =head1 DESCRIPTION
 
 This plugin is a C<Releaser> that does nothing. It is directed to plugin
@@ -41,3 +46,8 @@ When this plugin does the release, it will just log a message and finish.
 If you set the environment variable C<DZIL_FAKERELEASE_FAIL> to a true value,
 the plugin will die instead of doing nothing. This can be usefulfor
 authors wanting to test reliably that release failed.
+
+You can optionally provide the 'user' parameter, which defaults to 'AUTHORID',
+which will allow things that depend on this metadata
+( Sometimes provided by L<Dist::Zilla::Plugin::UploadToCPAN> ) to still work.
+( For example: L<Dist::Zilla::Plugin::Twitter> )
