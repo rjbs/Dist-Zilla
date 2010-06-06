c7ab721c3ce5554e6ccd0dc81fb2a7275e561f25
Florian Ragwitz <rafl@debian.org>
2010-06-06 16:16:22 +0200

    Allow a payload to be passed to filtered bundles

--- lib/Dist/Zilla/PluginBundle/Filter.pm
+++ lib/Dist/Zilla/PluginBundle/Filter.pm
@@ -11,28 +11,41 @@ use Dist::Zilla::Util;
 In your F<dist.ini>:
 
   [@Filter]
-  bundle = @Classic
-  remove = PodVersion
-  remove = Manifest
+  -bundle = @Classic
+  -remove = PodVersion
+  -remove = Manifest
+  option = for_classic
 
 =head1 DESCRIPTION
 
 This plugin bundle actually wraps and modified another plugin bundle.  It
-includes all the configuration for the bundle named in the C<bundle> attribute,
-but removes all the entries whose package is given in the C<remove> attributes.
+includes all the configuration for the bundle named in the C<-bundle> attribute,
+but removes all the entries whose package is given in the C<-remove> attributes.
+
+Options not prefixed with C<-> will be passed to the bundle to be filtered.
 
 =cut
 
-sub mvp_multivalue_args { qw(remove) }
+sub mvp_multivalue_args { qw(remove -remove) }
 
 sub bundle_config {
   my ($self, $section) = @_;
   my $class = (ref $self) || $self;
 
-  my $config = $section->{payload};
+  my $config = {};
+
+  my $has_filter_args = $section->{payload}->keys->grep(sub { /^-/ })->length;
+  for my $key ($section->{payload}->keys->flatten) {
+      my $val = $section->{payload}->{$key};
+      my $target = $has_filter_args && ($key !~ /^-/)
+        ? 'bundle'
+        : 'filter';
+      $key =~ s/^-// if $target eq 'filter';
+      $config->{$target}->{$key} = $val;
+  }
 
   Carp::croak("no bundle given for bundle filter")
-    unless my $bundle = $config->{bundle};
+    unless my $bundle = $config->{filter}->{bundle};
 
   $bundle = Dist::Zilla::Util->expand_config_package_name($bundle);
 
@@ -41,10 +54,10 @@ sub bundle_config {
   my @plugins = $bundle->bundle_config({
     name    => $section->{name}, # not 100% sure about this -- rjbs, 2010-03-06
     package => $bundle,
-    payload => {},
+    payload => $config->{bundle} || {},
   });
 
-  return @plugins unless my $remove = $config->{remove};
+  return @plugins unless my $remove = $config->{filter}->{remove};
 
   require List::MoreUtils;
   for my $i (reverse 0 .. $#plugins) {
