de1fef7f06834f9a0422d03e9335632921279f20
Ricardo Signes <rjbs@cpan.org>
2010-06-11 09:53:36 -0400

    the great new PkgDist

---
+++ lib/Dist/Zilla/Plugin/PkgDist.pm
@@ -0,0 +1,112 @@
+package Dist::Zilla::Plugin::PkgDist;
+# ABSTRACT: add a $DIST to your packages
+use Moose;
+with(
+  'Dist::Zilla::Role::FileMunger',
+  'Dist::Zilla::Role::FileFinderUser' => {
+    default_finders => [ ':InstallModules', ':ExecFiles' ],
+  },
+);
+
+use PPI;
+
+=head1 DESCRIPTION
+
+This plugin will add a line like the following to each package in each Perl
+module or program (more or less) within the distribution:
+
+  { our $DIST = 'My-CPAN-Dist'; } # where 'My-CPAN-Dist' is your dist name
+
+It will skip any package declaration that includes a newline between the
+C<package> keyword and the package name, like:
+
+  package
+    Foo::Bar;
+
+This sort of declaration is also ignored by the CPAN toolchain, and is
+typically used when doing monkey patching or other tricky things.
+
+=cut
+
+sub munge_files {
+  my ($self) = @_;
+
+  $self->munge_file($_) for @{ $self->found_files };
+}
+
+sub munge_file {
+  my ($self, $file) = @_;
+
+  # XXX: for test purposes, for now! evil! -- rjbs, 2010-03-17
+  return                          if $file->name    =~ /^corpus\//;
+
+  return                          if $file->name    =~ /\.t$/i;
+  return $self->munge_perl($file) if $file->name    =~ /\.(?:pm|pl)$/i;
+  return $self->munge_perl($file) if $file->content =~ /^#!(?:.*)perl(?:$|\s)/;
+  return;
+}
+
+sub munge_perl {
+  my ($self, $file) = @_;
+
+  my $dist_name = $self->zilla->name;
+
+  my $content = $file->content;
+
+  my $document = PPI::Document->new(\$content)
+    or Carp::croak( PPI::Document->errstr );
+
+  {
+    # This is sort of stupid.  We want to see if we assign to $DIST already.
+    # I'm sure there's got to be a better way to do this, but what the heck --
+    # this should work and isn't too slow for me. -- rjbs, 2009-11-29
+    my $code_only = $document->clone;
+    $code_only->prune("PPI::Token::$_") for qw(Comment Pod Quote Regexp);
+    if ($code_only->serialize =~ /\$DIST\s*=/sm) {
+      $self->log([ 'skipping %s: assigns to $DIST', $file->name ]);
+      return;
+    }
+  }
+
+  return unless my $package_stmts = $document->find('PPI::Statement::Package');
+
+  my %seen_pkg;
+
+  for my $stmt (@$package_stmts) {
+    my $package = $stmt->namespace;
+
+    if ($seen_pkg{ $package }++) {
+      $self->log([ 'skipping package re-declaration for %s', $package ]);
+      next;
+    }
+
+    if ($stmt->content =~ /package\s*\n\s*\Q$package/) {
+      $self->log([ 'skipping private package %s', $package ]);
+      next;
+    }
+
+    # the \x20 hack is here so that when we scan *this* document we don't find
+    # an assignment to version; it shouldn't be needed, but it's been annoying
+    # enough in the past that I'm keeping it here until tests are better
+    my $perl = "BEGIN {\n  \$$package\::DIST\x20=\x20'$dist_name';\n}\n";
+
+    my $dist_doc = PPI::Document->new(\$perl);
+    my @children = $dist_doc->schildren;
+
+    $self->log_debug([
+      'adding $DIST assignment to %s in %s',
+      $package,
+      $file->name,
+    ]);
+
+    Carp::carp('error inserting $DIST in ' . $file->name)
+      unless $stmt->insert_after($children[0]->clone)
+      and    $stmt->insert_after( PPI::Token::Whitespace->new("\n") );
+  }
+
+  $file->content($document->serialize);
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
---
+++ t/plugins/pkgdist.t
@@ -0,0 +1,147 @@
+use strict;
+use warnings;
+use Test::More 0.88;
+
+use lib 't/lib';
+
+use autodie;
+use Test::DZil;
+
+my $with_version = '
+package DZT::WVer;
+our $DIST = \'DZT-Blort\';
+1;
+';
+
+my $two_packages = '
+package DZT::TP1;
+
+package DZT::TP2;
+
+1;
+';
+
+my $repeated_packages = '
+package DZT::R1;
+
+package DZT::R2;
+
+package DZT::R1;
+
+1;
+';
+
+my $monkey_patched = '
+package DZT::TP1;
+
+package
+ DZT::TP2;
+
+1;
+';
+
+my $script = '
+#!/usr/bin/perl
+
+print "hello world\n";
+';
+
+my $script_pkg = '
+#!/usr/bin/perl
+
+package DZT::Script;
+';
+
+my $tzil = Dist::Zilla::Tester->from_config(
+  { dist_root => 'corpus/DZT' },
+  {
+    add_files => {
+      'source/lib/DZT/TP1.pm'    => $two_packages,
+      'source/lib/DZT/WVer.pm'   => $with_version,
+      'source/lib/DZT/R1.pm'     => $repeated_packages,
+      'source/lib/DZT/Monkey.pm' => $monkey_patched,
+      'source/bin/script_pkg.pl' => $script_pkg,
+      'source/bin/script_ver.pl' => $script_pkg . "our \$DIST = 'DZT-Blort';\n",
+      'source/bin/script.pl'     => $script,
+      'source/dist.ini' => simple_ini('GatherDir', 'PkgDist', 'ExecDir'),
+    },
+  },
+);
+
+$tzil->build;
+
+my $dzt_sample = $tzil->slurp_file('build/lib/DZT/Sample.pm');
+like(
+  $dzt_sample,
+  qr{^\s*\$\QDZT::Sample::DIST = 'DZT-Sample';\E$}m,
+  "added version to DZT::Sample",
+);
+
+my $dzt_tp1 = $tzil->slurp_file('build/lib/DZT/TP1.pm');
+like(
+  $dzt_tp1,
+  qr{^\s*\$\QDZT::TP1::DIST = 'DZT-Sample';\E$}m,
+  "added version to DZT::TP1",
+);
+
+like(
+  $dzt_tp1,
+  qr{^\s*\$\QDZT::TP2::DIST = 'DZT-Sample';\E$}m,
+  "added version to DZT::TP2",
+);
+
+my $dzt_wver = $tzil->slurp_file('build/lib/DZT/WVer.pm');
+unlike(
+  $dzt_wver,
+  qr{^\s*\$\QDZT::WVer::DIST = 'DZT-Sample';\E$}m,
+  "*not* added to DZT::WVer; we have one already",
+);
+
+my $dzt_script_pkg = $tzil->slurp_file('build/bin/script_pkg.pl');
+like(
+    $dzt_script_pkg,
+    qr{^\s*\$\QDZT::Script::DIST = 'DZT-Sample';\E$}m,
+    "added version to DZT::Script",
+);
+
+TODO: {
+    local $TODO = 'only scanning for packages right now';
+    my $dzt_script = $tzil->slurp_file('build/bin/script.pl');
+    like(
+        $dzt_script,
+        qr{^\s*\$\QDZT::Script::DIST = 'DZT-Sample';\E$}m,
+        "added version to plain script",
+    );
+};
+
+my $script_wver = $tzil->slurp_file('build/bin/script_ver.pl');
+unlike(
+    $script_wver,
+    qr{^\s*\$\QDZT::WVer::DIST = 'DZT-Sample';\E$}m,
+    "*not* added to versioned DZT::Script; we have one already",
+);
+
+ok(
+  grep({ m(skipping lib/DZT/WVer\.pm: assigns to \$DIST) }
+    @{ $tzil->log_messages }),
+  "we report the reason for no updateing WVer",
+);
+
+my $dzt_r1 = $tzil->slurp_file('build/lib/DZT/R1.pm');
+my @matches = grep { /R1::DIST/ } split /\n/, $dzt_r1;
+is(@matches, 1, "we add at most 1 DIST per package");
+
+my $dzt_monkey = $tzil->slurp_file('build/lib/DZT/Monkey.pm');
+unlike(
+  $dzt_monkey,
+  qr{\$DZT::TP2::DIST},
+  "no version for DZT::TP2 when it looks like a monkey patch"
+);
+
+ok(
+  grep({ m(skipping .+ DZT::TP2) } @{ $tzil->log_messages }),
+  "we report the reason for not updating Monkey",
+);
+
+done_testing;
+
