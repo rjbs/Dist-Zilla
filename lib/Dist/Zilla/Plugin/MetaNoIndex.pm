5240ef66cc829a46687f12042fa963a3e1e57e45
Ricardo Signes <rjbs@cpan.org>
2011-02-08 19:01:51 -0500

    import MetaNoIndex, with some significant overhauls

---
+++ lib/Dist/Zilla/Plugin/MetaNoIndex.pm
@@ -0,0 +1,113 @@
+package Dist::Zilla::Plugin::MetaNoIndex;
+# ABSTRACT: Stop CPAN from indexing stuff
+
+use Moose;
+with 'Dist::Zilla::Role::MetaProvider';
+
+=encoding utf8
+
+=head1 SYNOPSIS
+
+In your F<dist.ini>:
+
+  [MetaNoIndex]
+
+  directory = t/author
+  directory = examples
+
+  file = lib/Foo.pm
+
+  package = My::Module
+
+  namespace = My::Module
+
+=head1 DESCRIPTION
+
+This plugin allows you to prevent PAUSE/CPAN from indexing files you don't
+want indexed. This is useful if you build test classes or example classes
+that are used for those purposes only, and are not part of the distribution.
+It does this by adding a C<no_index> block to your F<META.json> (or
+F<META.yml>) file in your distribution.
+
+=for Pod::Coverage mvp_aliases mvp_multivalue_args
+
+=cut
+
+my %ATTR_ALIAS = (
+  directories => [ qw(directory dir folder) ],
+  files       => [ qw(file) ],
+  packages    => [ qw(package class module) ],
+  namespaces  => [ qw(namespace) ],
+);
+
+sub mvp_aliases {
+  my %alias_for;
+  
+  for my $key (keys %ATTR_ALIAS) {
+    $alias_for{ $_ } = $key for @{ $ATTR_ALIAS{$key} };
+  }
+
+  return \%alias_for;
+}
+
+sub mvp_multivalue_args { return keys %ATTR_ALIAS }
+
+=attr directories
+
+Exclude folders and everything in them, for example: F<author.t>
+
+Aliases: C<folder>, C<dir>, C<directory>
+
+=attr files
+
+Exclude a specific file, for example: F<lib/Foo.pm>
+
+Alias: C<file>
+
+=attr packages
+
+Exclude by package name, for example: C<My::Package>
+
+Aliases: C<class>, C<module>, C<package>
+
+=attr namespaces
+
+Exclude everything under a specific namespace, for example: C<My::Package>
+
+Alias: C<namespace>
+
+B<NOTE:> This will not exclude the package C<My::Package>, only everything
+under it like C<My::Package::Foo>.
+
+=cut
+
+for my $attr (keys %ATTR_ALIAS) {
+  has $attr => (
+    is        => 'ro',
+    isa       => 'ArrayRef[Str]',
+    init_arg  => $attr,
+    predicate => "_has_$attr",
+  );
+}
+
+=method metadata
+
+Returns a reference to a hash containing the distribution's no_index metadata.
+
+=cut
+
+sub metadata {
+  my $self = shift;
+  return {
+    no_index => {
+      map  {; my $reader = $_->[0];  ($_->[1] => $self->$reader) }
+      grep {; my $pred   = "_has_$_->[0]"; $self->$pred }
+      map  {; [ $_ => $ATTR_ALIAS{$_}[0] ] }
+      keys %ATTR_ALIAS
+    }
+  };
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
---
+++ t/plugins/metanoindex.t
@@ -0,0 +1,74 @@
+#!perl
+use strict;
+use warnings;
+
+use Test::More;
+use Test::Deep;
+use Test::DZil;
+
+my %TEST_ATTR = (
+  files => {
+    values  => [ qw(My/Module.pm My/Module2.pm) ],
+    aliases => [ qw(files) ],
+  },
+  directories => {
+    values  => [ qw(My/Private My/Private2) ],
+    aliases => [ qw(dir directory folder) ],
+  },
+  packages => {
+    values  => [ qw(My::Module::Stuff My::Module::Things) ],
+    aliases => [ qw(class module package) ],
+  },
+  namespaces => {
+    values  => [ qw(My::Module::Stuff My::Module::Things)],
+    aliases => [ qw(namespace) ],
+  },
+);
+
+
+my $tzil = Builder->from_config(
+  { dist_root => 'corpus/dist/DZT' },
+  {
+    add_files => {
+      'source/dist.ini' => simple_ini(
+        'MetaJSON',
+        [
+          MetaNoIndex => {
+            file  => 'file-1.txt',
+            files => 'file-2.txt',
+            
+            dir         => 'dir-1',
+            directory   => 'dir-2',
+            directories => 'dir-3',
+            folder      => 'dir-4',
+
+            package  => 'Package::1',
+            packages => 'Package::2',
+            class    => 'Class::1',
+            module   => 'Module::1',
+
+            namespace  => 'Namespace::1',
+            namespaces => 'Namespaces::1',
+          },
+        ],
+      ),
+    },
+  },
+);
+
+$tzil->build;
+
+my $meta = $tzil->slurp_file('build/META.json');
+
+is_json(
+  $meta,
+  superhashof({ no_index => {
+    file      => bag(qw(file-1.txt file-2.txt)),
+    directory => bag(qw(dir-1 dir-2 dir-3 dir-4)),
+    package   => bag(qw(Package::1 Package::2 Class::1 Module::1)),
+    namespace => bag(qw(Namespace::1 Namespaces::1)),
+  }}),
+  "we generated the no_index entry we expected",
+);
+
+done_testing;
