c0e021d1fe2a8ff956c10ab4b420c9a3a3a03b4f
Jesse Luehrs <doy@tozt.net>
2011-04-17 15:55:03 -0500

    allow listing only missing dependencies in listdeps

--- lib/Dist/Zilla/App/Command/listdeps.pm
+++ lib/Dist/Zilla/App/Command/listdeps.pm
@@ -14,7 +14,8 @@ This is a command plugin for L<Dist::Zilla>. It provides the C<listdeps>
 command, which prints your distribution's prerequisites. You could pipe that
 list to a CPAN client like L<cpan> to install all of the dependecies in one
 quick go. This will include author dependencies (those listed under
-C<develop_requires>) if the C<--author> flag is passed.
+C<develop_requires>) if the C<--author> flag is passed. You can also pass the
+C<--missing> flag to list only dependencies which are unsatisfied.
 
 =head1 ACKNOWLEDGEMENTS
 
@@ -25,16 +26,18 @@ API.
 =cut
 
 use Moose::Autobox;
+use Try::Tiny;
 use Version::Requirements;
 
 sub abstract { "print your distribution's prerequisites" }
 
 sub opt_spec {
   [ 'author', 'include author dependencies' ],
+  [ 'missing', 'list only the missing dependencies' ],
 }
 
 sub extract_dependencies {
-  my ($self, $zilla, $phases) = @_;
+  my ($self, $zilla, $phases, $missing) = @_;
 
   $_->before_build for $zilla->plugins_with(-BeforeBuild)->flatten;
   $_->gather_files for $zilla->plugins_with(-FileGatherer)->flatten;
@@ -49,7 +52,14 @@ sub extract_dependencies {
     $req->add_requirements( $prereqs->requirements_for($phase, 'requires') );
   }
 
-  return sort { lc $a cmp lc $b } grep { $_ ne 'perl' } $req->required_modules;
+  my @required = grep { $_ ne 'perl' } $req->required_modules;
+  if ($missing) {
+    @required = grep { !( try { Class::MOP::load_class($_); 1 }
+                       && $req->accepts_module($_ => $_->VERSION) ) }
+                     @required;
+  }
+
+  return sort { lc $a cmp lc $b } @required;
 }
 
 sub execute {
@@ -60,7 +70,8 @@ sub execute {
   my @phases = qw(build test configure runtime);
   push @phases, 'develop' if $opt->author;
 
-  print "$_\n" for $self->extract_dependencies($self->zilla, \@phases);
+  print "$_\n"
+    for $self->extract_dependencies($self->zilla, \@phases, $opt->missing);
 }
 
 1;
