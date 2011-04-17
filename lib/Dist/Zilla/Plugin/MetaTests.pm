9e4f0eee89377dcdd11bd9a63adf682f25b80ed2
Mike Doherty <doherty@cs.dal.ca>
2011-04-17 18:07:34 -0300

    Prune distmeta.t when META.yml doesn't exist

--- lib/Dist/Zilla/Plugin/MetaTests.pm
+++ lib/Dist/Zilla/Plugin/MetaTests.pm
@@ -1,16 +1,38 @@
 package Dist::Zilla::Plugin::MetaTests;
 # ABSTRACT: common extra tests for META.yml
 use Moose;
+use Moose::Autobox;
 extends 'Dist::Zilla::Plugin::InlineFiles';
+with 'Dist::Zilla::Role::FilePruner';
 
 =head1 DESCRIPTION
 
 This is an extension of L<Dist::Zilla::Plugin::InlineFiles>, providing the
-following files:
+following file if F<META.yml> is in your dist:
 
   xt/release/meta-yaml.t - a standard Test::CPAN::Meta test
 
 =cut
+
+sub prune_files {
+    my $self = shift;
+
+    # Bail if we find META.yml
+    my $METAyml = 'META.yml';
+    foreach my $file ($self->zilla->files->flatten) {
+        return if $file->name eq $METAyml;
+    }
+
+    # If META.yml wasn't found, then prune out the test
+    my $test_filename = 'xt/release/distmeta.t';
+    foreach my $file ($self->zilla->files->flatten) {
+        next unless $file->name eq $test_filename;
+
+        $self->zilla->prune_file($file);
+        $self->log_debug([ '%s not found; pruning %s', $METAyml, $file->name ]);
+    }
+    return;
+}
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
