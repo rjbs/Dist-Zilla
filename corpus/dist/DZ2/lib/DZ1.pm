6b3c18dbe935dee8fd331bf3366f375dc2e50976
Ricardo Signes <rjbs@cpan.org>
2011-11-18 10:42:08 -0500

    document the "why" of the last commit

--- corpus/dist/DZ2/lib/DZ1.pm
+++ corpus/dist/DZ2/lib/DZ1.pm
@@ -13,4 +13,11 @@ __END__
 
 DZ1 - this is a sample package for testing Dist::Zilla;
 
+=head1 HEY, MAINTAINER
+
+Note that we have C<< =head1 NAME >> here, and not an ABSTRACT comment.  That's
+to exercize C<abstract_from_file>.
+
+  -- rjbs, 2011-11-18
+
 =cut
