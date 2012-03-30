7791bbaf03e8f94c3b02989ade69d9a83d14550f
Ricardo Signes <rjbs@cpan.org>
2012-03-30 11:42:43 +0200

    do not claim that a VERSION-less dep is missing

--- lib/Dist/Zilla/App/Command/listdeps.pm
+++ lib/Dist/Zilla/App/Command/listdeps.pm
@@ -61,13 +61,12 @@ sub extract_dependencies {
       my $mod = shift;
       # it is required if it's not already installed
       return 1 unless Class::Load::try_load_class($mod);
-      # it is required if the version can't be parsed
-      # right now $_->VERSION by itself will die if the version can't
-      # be parsed, but this may change in the future to only die in that
-      # way if an arg is passed
-      return 1 if ! try { $mod->VERSION(0); 1 };
-      # it is required if the version doesn't meet the requirement
-      return !$req->accepts_module($mod => $mod->VERSION);
+
+      # guard against $VERSION = -1 and other insanity
+      my $version;
+      return unless try { $version = $mod->VERSION; 1; };
+
+      return !$req->accepts_module($mod => $version);
     };
     @required = grep { $is_required->($_) } @required;
   }
