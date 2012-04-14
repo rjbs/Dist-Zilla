2f6d7ba87bb9286feea8a77d1e71c5f06c65e6fc
Karen Etheridge <ether@cpan.org>
2012-04-14 11:49:16 -0700

    --dry-run option for dzil clean

--- lib/Dist/Zilla/App/Command/clean.pm
+++ lib/Dist/Zilla/App/Command/clean.pm
@@ -6,7 +6,7 @@ use Dist::Zilla::App -command;
 
 =head1 SYNOPSIS
 
-  dzil clean
+  dzil clean [ --dry-run|-n ]
 
 This command removes some files that are created during build, test, and
 install.  It's a very thin layer over the C<L<clean|Dist::Zilla/clean>> method
@@ -15,12 +15,24 @@ information about the files that will be removed.
 
 =cut
 
+sub opt_spec {
+  [ 'dry-run|n'   => 'don\'t actually remove anything, just show what would be done' ],
+}
+
+=head1 OPTIONS
+
+=head2 -n, --dry-run
+
+Nothing is removed; instead, everything that would be removed will be listed.
+
+=cut
+
 sub abstract { 'clean up after build, test, or install' }
 
 sub execute {
   my ($self, $opt, $arg) = @_;
 
-  $self->zilla->clean;
+  $self->zilla->clean($opt->dry_run);
 }
 
 1;
