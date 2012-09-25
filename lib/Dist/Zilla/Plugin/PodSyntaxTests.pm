f88e22e12ee294285e392fdf7b083c2196b02cee
Olivier Mengué <dolmen@cpan.org>
2012-09-25 15:42:26 +0200

    RT79842: PodSyntaxTests now injects Test::Pod dependency
    Fix for RT#79842: https://rt.cpan.org/Ticket/Display.html?id=79842
    A "develop requires" dependency on Test::Pod 1.41 is injected if
    PodSyntaxTests is used.

--- lib/Dist/Zilla/Plugin/PodSyntaxTests.pm
+++ lib/Dist/Zilla/Plugin/PodSyntaxTests.pm
@@ -2,6 +2,7 @@ package Dist::Zilla::Plugin::PodSyntaxTests;
 # ABSTRACT: a release test for Pod syntax
 use Moose;
 extends 'Dist::Zilla::Plugin::InlineFiles';
+with 'Dist::Zilla::Role::PrereqSource';
 
 use namespace::autoclean;
 
@@ -12,7 +13,25 @@ following files:
 
   xt/release/pod-syntax.t   - a standard Test::Pod test
 
+L<Test::Pod> C<1.41> will be added as a C<develop requires> dependency.
+
 =cut
+
+
+# Register the release test prereq as a "develop requires"
+# so it will be listed in "dzil listdeps --author"
+sub register_prereqs {
+  my ($self) = @_;
+
+  $self->zilla->register_prereqs(
+    {
+      type  => 'requires',
+      phase => 'develop',
+    },
+    'Test::Pod' => '1.41',
+  );
+}
+
 
 __PACKAGE__->meta->make_immutable;
 1;
