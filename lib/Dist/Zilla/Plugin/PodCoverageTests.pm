5efbc7fa25591d82ebe7d8c13606cff8754d9f06
Ricardo Signes <rjbs@cpan.org>
2012-10-08 08:13:20 -0400

    make PodCoverageTests register prereqs too

--- lib/Dist/Zilla/Plugin/PodCoverageTests.pm
+++ lib/Dist/Zilla/Plugin/PodCoverageTests.pm
@@ -2,6 +2,7 @@ package Dist::Zilla::Plugin::PodCoverageTests;
 # ABSTRACT: a release test for Pod coverage
 use Moose;
 extends 'Dist::Zilla::Plugin::InlineFiles';
+with 'Dist::Zilla::Role::PrereqSource';
 
 use namespace::autoclean;
 
@@ -19,6 +20,21 @@ documentation can be found, you can add:
   =for Pod::Coverage sub_name other_sub this_one_too
 
 =cut
+
+# Register the release test prereq as a "develop requires"
+# so it will be listed in "dzil listdeps --author"
+sub register_prereqs {
+  my ($self) = @_;
+
+  $self->zilla->register_prereqs(
+    {
+      type  => 'requires',
+      phase => 'develop',
+    },
+    'Test::Pod::Coverage'     => '1.08',
+    'Pod::Coverage::TrustPod' => 0,
+  );
+}
 
 __PACKAGE__->meta->make_immutable;
 1;
