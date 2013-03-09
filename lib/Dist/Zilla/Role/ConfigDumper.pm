8e06ae13140639f79600d909a9c0493c7ff425ec
Yanick Champoux <yanick@babyl.dyndns.org>
2013-03-08 23:11:14 -0500

    typo in configuration

--- lib/Dist/Zilla/Role/ConfigDumper.pm
+++ lib/Dist/Zilla/Role/ConfigDumper.pm
@@ -1,5 +1,5 @@
 package Dist::Zilla::Role::ConfigDumper;
-# ABSTRACT: something that can dump its (public, simplified) configuraiton
+# ABSTRACT: something that can dump its (public, simplified) configuration
 use Moose::Role;
 
 use namespace::autoclean;
