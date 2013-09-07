a20c0be8efdfd2d7f1b35790f4e8813859a18893
Olivier Mengué <dolmen@cpan.org>
2013-09-08 00:06:51 +0200

    PodSyntaxTests test: unconditionnally load dependencies
    This is a release test. Any missing release test dependency must be
    handled as a failure of the test, not silently skipped.
    And by the way, this case should not even happen as the develop/require
    dependency is properly injected by the plugin, so the developer should
    have installed them with 'dzil listdeps --author | cpanm'.

--- lib/Dist/Zilla/Plugin/PodSyntaxTests.pm
+++ lib/Dist/Zilla/Plugin/PodSyntaxTests.pm
@@ -39,9 +39,7 @@ __PACKAGE__->meta->make_immutable;
 __DATA__
 ___[ xt/release/pod-syntax.t ]___
 #!perl
-use Test::More;
 
-eval "use Test::Pod 1.41";
-plan skip_all => "Test::Pod 1.41 required for testing POD" if $@;
+use Test::Pod 1.41;
 
 all_pod_files_ok();
