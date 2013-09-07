bb94f4d27afcf3f514470a4175defbf280a0e8ab
Olivier Mengué <dolmen@cpan.org>
2013-09-08 00:04:07 +0200

    PodCoverageTests test: unconditionnally load dependencies
    This is a release test. Any missing release test dependency must be
    handled as a failure of the test, not silently skipped.
    And by the way, this case should not even happen as the develop/require
    dependency is properly injected by the plugin, so the developer should
    have installed them with 'dzil listdeps --author | cpanm'.

--- lib/Dist/Zilla/Plugin/PodCoverageTests.pm
+++ lib/Dist/Zilla/Plugin/PodCoverageTests.pm
@@ -43,14 +43,7 @@ __DATA__
 ___[ xt/release/pod-coverage.t ]___
 #!perl
 
-use Test::More;
-
-eval "use Test::Pod::Coverage 1.08";
-plan skip_all => "Test::Pod::Coverage 1.08 required for testing POD coverage"
-  if $@;
-
-eval "use Pod::Coverage::TrustPod";
-plan skip_all => "Pod::Coverage::TrustPod required for testing POD coverage"
-  if $@;
+use Test::Pod::Coverage 1.08;
+use Pod::Coverage::TrustPod;
 
 all_pod_coverage_ok({ coverage_class => 'Pod::Coverage::TrustPod' });
