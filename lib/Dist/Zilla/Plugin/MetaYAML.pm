34bad6b617992d9041d01de40acea3fe0ad4d01d
Ricardo Signes <rjbs@cpan.org>
2013-09-07 22:26:21 -0400

    consider somewhat more painstakingly how to encode

--- lib/Dist/Zilla/Plugin/MetaYAML.pm
+++ lib/Dist/Zilla/Plugin/MetaYAML.pm
@@ -4,6 +4,7 @@ use Moose;
 use Moose::Autobox;
 with 'Dist::Zilla::Role::FileGatherer';
 
+use Encode qw(encode_utf8 decode_utf8 FB_CROAK);
 use namespace::autoclean;
 
 =head1 DESCRIPTION
@@ -71,9 +72,48 @@ sub gather_files {
 
       my $converter = CPAN::Meta::Converter->new($distmeta);
       my $output    = $converter->convert(version => $self->version);
+      my $yaml      = YAML::Tiny::Dump($output);
 
-      utf8::encode( my $guts = YAML::Tiny::Dump($output) );
-      return $guts;
+      # Okay, this code is all stuff that makes rjbs cry, but it's all his
+      # fault, too, because he wasn't really strict about text vs. bytes to
+      # begin with.  Why not?  Because he was lazy.  He is very sorry, please
+      # do not be too hard with him.  He mostly lives in an ASCII neighborhood,
+      # and sometimes doesn't notice his own privilege.
+      #
+      # There are a few possibilities:
+      #   1) It's all ASCII.  Whatevs.
+      #   2) It's code points 0x00 to 0xFF and...
+      #     a) it's Latin-1/Unicode text
+      #     b) it's UTF-8
+      #   3) It has code points above 0xFF and must be Unicode.
+      #   4) Some stupid nonsense that I don't care about.
+      #
+      # In (1) we output the codepoints as is.
+      # In (3) we can encode to UTF-8 and feel pretty good about that.
+      # In (4) I don't give a darn.
+      #
+      # Between the other cases, the question is: did we get text or octets?
+      # We *should* be sure that it's all text, but right now it's usually
+      # octets.  If the document is a valid UTF-8 string, we emit it as is.  If
+      # it's not, we assume it's text and encode it to UTF-8.
+      #
+      # -- rjbs, 2013-09-07
+      if (grep {; /\P{ASCII}/ } $yaml) {
+        if (
+          $yaml =~ /[^\x00-\xFF]/
+          or
+          ! eval { decode_utf8($yaml, FB_CROAK); 1 }
+        ) {
+          # Characters over \xFF or not a valid UTF-8 buffer:
+          # assume it's all text.
+          $yaml = encode_utf8($yaml);
+        } else {
+          # It's already valid UTF-8.  Emit it as is.
+          $yaml = encode_utf8($yaml);
+        }
+      }
+
+      return $yaml;
     },
   });
 
