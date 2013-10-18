00f902d21494393fd04b2b2f3c5dcc85e9b372ba
Karen Etheridge <ether@cpan.org>
2013-10-17 18:39:52 -0700

    remove unused lib includes

--- lib/Dist/Zilla/Plugin/ModuleBuild.pm
+++ lib/Dist/Zilla/Plugin/ModuleBuild.pm
@@ -1,7 +1,6 @@
 package Dist::Zilla::Plugin::ModuleBuild;
 # ABSTRACT: build a Build.PL that uses Module::Build
 
-use List::MoreUtils qw(any uniq);
 use Moose;
 use Moose::Autobox;
 with (
@@ -14,7 +13,6 @@ use namespace::autoclean;
 
 use CPAN::Meta::Requirements 2.121; # requirements_for_module
 use Dist::Zilla::File::InMemory;
-use List::MoreUtils qw(any uniq);
 use Data::Dumper;
 
 =head1 DESCRIPTION
