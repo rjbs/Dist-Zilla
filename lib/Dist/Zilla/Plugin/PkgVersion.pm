dc3d2851df488c4512a16be17c8510a179cbcd49
Karen Etheridge <ether@cpan.org>
2013-11-02 20:55:25 -0700

    munge all found files in [PkgVersion], not a subset
    A filefinder was already being used, but undocumented.  Also only .pm/.pl
    files (or files with a shebang) were being munged, which excludes .pod files
    -- only files with a package declaration will be modified, as before.
    
    Closes PRs #219 and #233.
    
    Conflicts:
    	Changes
    	lib/Dist/Zilla/Plugin/PkgVersion.pm

--- lib/Dist/Zilla/Plugin/PkgVersion.pm
+++ lib/Dist/Zilla/Plugin/PkgVersion.pm
@@ -56,6 +56,20 @@ will have different line numbers (off by one) than the source.  If
 C<die_on_line_insertion> is true, PkgVersion will raise an exception rather
 than insert a new line.
 
+=attr finder
+
+=for stopwords FileFinder
+
+This is the name of a L<FileFinder|Dist::Zilla::Role::FileFinder> for finding
+modules to edit.  The default value is C<:InstallModules> and C<:ExecFiles>;
+this option can be used more than once.
+
+Other predefined finders are listed in
+L<Dist::Zilla::Role::FileFinderUser/default_finders>.
+You can define your own with the
+L<[FileFinder::ByName]|Dist::Zilla::Plugin::FileFinder::ByName> and
+L<[FileFinder::Filter]|Dist::Zilla::Plugin::FileFinder::Filter> plugins.
+
 =cut
 
 sub munge_files {
@@ -67,16 +81,12 @@ sub munge_files {
 sub munge_file {
   my ($self, $file) = @_;
 
-  return                          if $file->name    =~ /\.t$/i;
-  return $self->munge_perl($file) if $file->name    =~ /\.(?:pm|pl)$/i;
-
   if ($file->is_bytes) {
     $self->log_debug($file->name . " has 'bytes' encoding, skipping...");
     return;
   }
 
-  return $self->munge_perl($file) if $file->content =~ /^#!(?:.*)perl(?:$|\s)/;
-  return;
+  return $self->munge_perl($file);
 }
 
 has die_on_existing_version => (
@@ -110,7 +120,11 @@ sub munge_perl {
     return;
   }
 
-  return unless my $package_stmts = $document->find('PPI::Statement::Package');
+  my $package_stmts = $document->find('PPI::Statement::Package');
+  unless ($package_stmts) {
+    $self->log([ 'skipping %s: no package statement found', $file->name ]);
+    return;
+  }
 
   my %seen_pkg;
 
