857e4d4c9e16710c914d28c58937a859fe5443ec
David Golden <dagolden@cpan.org>
2014-03-16 13:41:59 +0100

    Add 'jobs' argument for 'dzil test'
    This passes a jobs argument to TestRunners so they can set harness
    options (or whatever) accordingly.

--- lib/Dist/Zilla/Plugin/MakeMaker/Runner.pm
+++ lib/Dist/Zilla/Plugin/MakeMaker/Runner.pm
@@ -28,10 +28,16 @@ sub build {
 }
 
 sub test {
-  my ( $self, $target ) = @_;
+  my ( $self, $target, $arg ) = @_;
 
   my $make = $self->make_path;
   $self->build;
+
+  my $jobs = $arg ? "j" . $arg->{jobs} : '';
+  my $ho = "HARNESS_OPTIONS";
+  local $ENV{$ho} = $ENV{$ho} ? "$ENV{$ho}:$jobs" : $jobs
+    if $jobs;
+
   system($make, 'test',
     ( $self->zilla->logger->get_debug ? 'TEST_VERBOSE=1' : () ),
   ) and die "error running $make test\n";
