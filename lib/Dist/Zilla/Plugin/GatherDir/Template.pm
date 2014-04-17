ce5cf100397e580cd3ddc63f5f9f2be7b9b79b63
Karen Etheridge <ether@cpan.org>
2014-04-17 13:05:48 -0700

    better error when file is unexpectedly missing
    This can happen when the gathering logic is different from "everything I see
    in the filesystem", e.g. in [Git::GatherDir] which gathers everything known to
    git, and the file isn't actually there.  Without this check, the user sees:
    
    Use of uninitialized value in bitwise and (&) at .../Dist/Zilla/Plugin/GatherDir.pm line 175.
    
    ..which doesn't tell the user exactly what went wrong.

--- lib/Dist/Zilla/Plugin/GatherDir/Template.pm
+++ lib/Dist/Zilla/Plugin/GatherDir/Template.pm
@@ -32,9 +32,11 @@ sub _file_from_filename {
 
   my $template = path($filename)->slurp_utf8;
 
+  my @stat = stat $filename or $self->log_fatal("$filename does not exist!");
+
   return Dist::Zilla::File::FromCode->new({
     name => $filename,
-    mode => ((stat $filename)[2] & 0755) | 0200, # kill world-writeability, make sure owner-writable.
+    mode => ($stat[2] & 0755) | 0200, # kill world-writeability, make sure owner-writable.
     code => sub {
       my ($file_obj) = @_;
       $self->fill_in_string(
