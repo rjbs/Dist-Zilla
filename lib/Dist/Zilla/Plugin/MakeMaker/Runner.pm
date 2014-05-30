b198e45a21cff10cd5684940faf61e1405820a2d
Karen Etheridge <ether@cpan.org>
2014-05-30 10:32:10 -0700

    issue some diagnostics under --verbose for build/test commands

--- lib/Dist/Zilla/Plugin/MakeMaker/Runner.pm
+++ lib/Dist/Zilla/Plugin/MakeMaker/Runner.pm
@@ -27,7 +27,10 @@ sub build {
   return
     if -e $makefile and (stat 'Makefile.PL')[9] <= (stat $makefile)[9];
 
+  $self->log_debug("running $^X Makefile.PL");
   system($^X => 'Makefile.PL') and die "error with Makefile.PL\n";
+
+  $self->log_debug("running $make");
   system($make)                and die "error running $make\n";
 
   return;
@@ -47,6 +50,7 @@ sub test {
   my $ho = "HARNESS_OPTIONS";
   local $ENV{$ho} = $ENV{$ho} ? "$ENV{$ho}:$jobs" : $jobs;
 
+  $self->log_debug(join(' ', "running $make test", ( $self->zilla->logger->get_debug ? 'TEST_VERBOSE=1' : () )));
   system($make, 'test',
     ( $self->zilla->logger->get_debug ? 'TEST_VERBOSE=1' : () ),
   ) and die "error running $make test\n";
