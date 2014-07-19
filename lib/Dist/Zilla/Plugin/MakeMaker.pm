c319758595818fec680117efdbc576249031979e
Graham Knop <haarg@haarg.org>
2014-07-19 16:24:50 -0400

    remove redundant code to calculate test/build prereqs

--- lib/Dist/Zilla/Plugin/MakeMaker.pm
+++ lib/Dist/Zilla/Plugin/MakeMaker.pm
@@ -191,17 +191,8 @@ sub write_makefile_args {
             ->as_string_hash;
   };
 
-  my $build_prereq
-    = $prereqs->requirements_for(qw(build requires))
-    ->clone
-    ->clear_requirement('perl')
-    ->as_string_hash;
-
-  my $test_prereq
-    = $prereqs->requirements_for(qw(test requires))
-    ->clone
-    ->clear_requirement('perl')
-    ->as_string_hash;
+  my $build_prereq = $prereqs_dump->(qw(build requires));
+  my $test_prereq = $prereqs_dump->(qw(test requires));
 
   my %write_makefile_args = (
     DISTNAME  => $self->zilla->name,
