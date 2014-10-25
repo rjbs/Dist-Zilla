87e1050feca41a09aa6344b30b1b9bca98745595
Karen Etheridge <ether@cpan.org>
2014-10-24 23:10:57 -0700

    document that after_release takes an argument

--- lib/Dist/Zilla/Role/AfterRelease.pm
+++ lib/Dist/Zilla/Role/AfterRelease.pm
@@ -9,7 +9,7 @@ use namespace::autoclean;
 =head1 DESCRIPTION
 
 Plugins implementing this role have their C<after_release> method called
-once the release is done.
+once the release is done. The archive filename is passed as the sole argument.
 
 =cut
 
