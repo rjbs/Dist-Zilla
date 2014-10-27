ae61b5fb10de21d28a4c79b6021e693cacab3ab8
Christian Walde <walde.christian@googlemail.com>
2014-10-27 22:36:17 +0100

    stop search after having found all elements on after-pkg line in PkgVersion
    This provides a measurable speed-up on dzil build on dzil itself.

--- lib/Dist/Zilla/Plugin/PkgVersion.pm
+++ lib/Dist/Zilla/Plugin/PkgVersion.pm
@@ -166,7 +166,8 @@ sub munge_perl {
         $document->flush_locations if $munged;
         my $curr_line_number = $curr->line_number + 1;
         my $find = $document->find(sub {
-          return $_[1]->line_number == $curr_line_number;
+          my $line = $_[1]->line_number;
+          return $line > $curr_line_number ? undef : $line == $curr_line_number;
         });
 
         last unless $find and @$find == 1;
