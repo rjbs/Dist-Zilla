rjbs/Dist-Zilla

commit e00f4ae1816ad5469318600bbd1e3b056badf9a5

the new ExtUtils::Manifest can finally handle quoted names

t/plugins/manifest.t
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 use Test::More 0.88;
 
-use ExtUtils::Manifest;
+use ExtUtils::Manifest 1.66; # or maniread can't cope with quoting properly
 
 use lib 't/lib';
 
@@ -13,9 +13,9 @@ my $tzil = Builder->from_config(
   {
     add_files => {
       q{source/file with spaces.txt}        => "foo\n",
-      # q{source/file\\with some\\whacks.txt} => "bar\n",
-      # q{source/'file-with-ticks.txt'}       => "baz\n",
-      # q{source/file'with'quotes\\or\\backslash.txt} => "quux\n",
+      q{source/file\\with some\\whacks.txt} => "bar\n",
+      q{source/'file-with-ticks.txt'}       => "baz\n",
+      q{source/file'with'quotes\\or\\backslash.txt} => "quux\n",
       'source/dist.ini' => simple_ini(
         'GatherDir',
         'Manifest',
@@ -33,9 +33,9 @@ is_deeply(
   [ sort(
     'MANIFEST',
     q{file with spaces.txt},
-    # q{file\\with some\\whacks.txt},
-    # q{file'with'quotes\\or\\backslash.txt},
-    # q{'file-with-ticks.txt'},
+    q{file\\with some\\whacks.txt},
+    q{file'with'quotes\\or\\backslash.txt},
+    q{'file-with-ticks.txt'},
     'dist.ini',
     'lib/DZT/Sample.pm',
     't/basic.t',
@@ -51,9 +51,9 @@ is_deeply(
   [ sort(
     'MANIFEST',
     q{'file with spaces.txt'},
-    # q{'file\\\\with some\\\\whacks.txt'},
-    # q{'\\'file-with-ticks.txt\\''},
-    # q{'file\\'with\\'quotes\\\\or\\\\backslash.txt'},
+    q{'file\\\\with some\\\\whacks.txt'},
+    q{'\\'file-with-ticks.txt\\''},
+    q{'file\\'with\\'quotes\\\\or\\\\backslash.txt'},
     'dist.ini',
     'lib/DZT/Sample.pm',
     't/basic.t',

commit e52a4f3873fccde72d108546807f2877ea2f896c

skip testing files in plugins/manifest on windows that have illegal paths
Fixes the Win32 failures introduced by
e00f4ae1816ad5469318600bbd1e3b056badf9a5

t/plugins/manifest.t
@@ -12,12 +12,14 @@ my $tzil = Builder->from_config(
   {
     add_files => {
       q{source/file with spaces.txt}        => "foo\n",
-      q{source/file\\with some\\whacks.txt} => "bar\n",
       q{source/'file-with-ticks.txt'}       => "baz\n",
-      q{source/file'with'quotes\\or\\backslash.txt} => "quux\n",
       'source/dist.ini' => simple_ini(
         'GatherDir',
         'Manifest',
+      ),
+      $^O eq "MSWin32" ? () : (
+        q{source/file\\with some\\whacks.txt} => "bar\n",
+        q{source/file'with'quotes\\or\\backslash.txt} => "quux\n",
       ),
     },
   },
@@ -32,12 +34,14 @@ cmp_deeply(
   bag(
     'MANIFEST',
     q{file with spaces.txt},
-    q{file\\with some\\whacks.txt},
-    q{file'with'quotes\\or\\backslash.txt},
     q{'file-with-ticks.txt'},
     'dist.ini',
     'lib/DZT/Sample.pm',
     't/basic.t',
+    $^O eq "MSWin32" ? () : (
+      q{file\\with some\\whacks.txt},
+      q{file'with'quotes\\or\\backslash.txt},
+    ),
   ),
   'manifest quotes files with spaces'
 );
@@ -50,12 +54,14 @@ cmp_deeply(
   bag(
     'MANIFEST',
     q{'file with spaces.txt'},
-    q{'file\\\\with some\\\\whacks.txt'},
     q{'\\'file-with-ticks.txt\\''},
-    q{'file\\'with\\'quotes\\\\or\\\\backslash.txt'},
     'dist.ini',
     'lib/DZT/Sample.pm',
     't/basic.t',
+    $^O eq "MSWin32" ? () : (
+      q{'file\\\\with some\\\\whacks.txt'},
+      q{'file\\'with\\'quotes\\\\or\\\\backslash.txt'},
+    ),
   ),
   'manifest quotes files with spaces'
 );