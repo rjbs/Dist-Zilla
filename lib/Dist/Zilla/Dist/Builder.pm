46d1b56ece73b8addce116e091b08f70e9b84a3b
Ricardo Signes <rjbs@cpan.org>
2015-03-07 17:13:17 -0500

    drop unreachable code
    The package name being tested is the package name, not
    the config section name, so it will never start with @.
    We can re-add this feature if it is made to work, which
    will require changes to Config::MVP.

--- lib/Dist/Zilla/Dist/Builder.pm
+++ lib/Dist/Zilla/Dist/Builder.pm
@@ -267,9 +267,8 @@ sub _load_config {
 
     my $package = $_->package;
 
-    my $bundle = $package =~ /^@/ ? ' bundle' : '';
     die <<"END_DIE";
-Required plugin$bundle $package isn't installed.
+Required plugin $package isn't installed.
 
 Run 'dzil authordeps' to see a list of all required plugins.
 You can pipe the list to your CPAN client to install or update them:
