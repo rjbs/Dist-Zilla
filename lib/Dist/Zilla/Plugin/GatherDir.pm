4d5196b02c7ca4bbd01312f42f9303e69550ea71
Karen Etheridge <ether@cpan.org>
2015-06-27 20:05:04 -0700

    sort these lists that appear in metadata
    This will improve the consistency between builds, which helps distribution
    packagers.

--- lib/Dist/Zilla/Plugin/GatherDir.pm
+++ lib/Dist/Zilla/Plugin/GatherDir.pm
@@ -153,8 +153,8 @@ around dump_config => sub {
   my $config = $self->$orig;
 
   $config->{+__PACKAGE__} = {
-    map { $_ => $self->$_ }
-      qw(root prefix include_dotfiles follow_symlinks exclude_filename exclude_match prune_directory),
+    (map { $_ => $self->$_ } qw(root prefix include_dotfiles follow_symlinks)),
+    (map { $_ => [ sort @{ $self->$_ } ] } qw(exclude_filename exclude_match prune_directory)),
   };
 
   return $config;
