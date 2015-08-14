cf1eb10a54dc6f9666ace74ead2e67e8dfb39139
Ricardo Signes <rjbs@cpan.org>
2015-08-13 23:03:46 -0400

    newer App::Cmd for reliable message formatting

--- lib/Dist/Zilla/App.pm
+++ lib/Dist/Zilla/App.pm
@@ -3,7 +3,7 @@ use warnings;
 package Dist::Zilla::App;
 # ABSTRACT: Dist::Zilla's App::Cmd
 
-use App::Cmd::Setup 0.309 -app; # better compilation error detection
+use App::Cmd::Setup 0.327 -app; # better compilation error detection
 
 use Carp ();
 use Try::Tiny;
