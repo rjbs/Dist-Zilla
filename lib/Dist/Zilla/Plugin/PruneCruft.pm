2a0cd0b488911a950b9e6dab0237dbeb5c9f11d9
Robert Rothenberg <rrwo@cpan.org>
2025-05-08 13:11:53 +0100

    PruneCruft prune perltidy.ERR files

--- lib/Dist/Zilla/Plugin/PruneCruft.pm
+++ lib/Dist/Zilla/Plugin/PruneCruft.pm
@@ -71,6 +71,7 @@ sub exclude_file {
   return 1 if $file->name =~ /\A_Inline/;
   return 1 if $file->name eq 'MYMETA.yml';
   return 1 if $file->name eq 'MYMETA.json';
+  return 1 if substr($file->name, -12) eq 'perltidy.ERR';
   return 1 if $file->name eq 'pm_to_blib';
   return 1 if substr($file->name, 0, 6) eq '_eumm/';
   # Avoid bundling fatlib/ dir created by App::FatPacker
