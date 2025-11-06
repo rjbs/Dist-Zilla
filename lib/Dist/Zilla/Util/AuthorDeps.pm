c5ccbf66d16b507acd42be76bef060c565a03cff
Ricardo Signes <rjbs@semiotic.systems>
2025-11-06 15:06:27 -0500

    dzil authordeps: add --cpanm-versions
    This also adds a `_` to a few subroutines, as they are not documented.

--- lib/Dist/Zilla/Util/AuthorDeps.pm
+++ lib/Dist/Zilla/Util/AuthorDeps.pm
@@ -9,19 +9,23 @@ use List::Util 1.45 ();
 
 use namespace::autoclean;
 
-sub format_author_deps {
-  my ($reqs, $versions) = @_;
+sub _format_author_deps {
+  my ($reqs, $versions, $cpanm_versions) = @_;
 
   my $formatted = '';
-  foreach my $rec (@{ $reqs }) {
-    my ($mod, $ver) = each(%{ $rec });
-    $formatted .= $versions ? "$mod = $ver\n" : "$mod\n";
+  for my $rec (@{ $reqs }) {
+    my ($mod, $ver) = %$rec;
+    $formatted .= $cpanm_versions ? qq{$mod~"$ver"\n}
+                : $versions       ? "$mod = $ver\n"
+                :                   "$mod\n";
   }
-  chomp($formatted);
+
+  chomp $formatted;
+
   return $formatted;
 }
 
-sub extract_author_deps {
+sub _extract_author_deps {
   my ($root, $missing) = @_;
 
   my $ini = path($root, 'dist.ini');
